d40ae68b672512a31eb9593606bbd8839f015639
John Wang <jwang@proofpoint.com>
2015-05-29 15:34:14 -0700

    add stringsutil.PadLeft and stringsutil.PadRight

---
+++ strings/stringsutil/stringsutil.go
@@ -0,0 +1,19 @@
+package stringsutil
+
+func PadRight(str string, pad string, length int) string {
+	for {
+		str += pad
+		if len(str) > length {
+			return str[0:length]
+		}
+	}
+}
+
+func PadLeft(str string, pad string, length int) string {
+	for {
+		str = pad + str
+		if len(str) > length {
+			return str[0:length]
+		}
+	}
+}
