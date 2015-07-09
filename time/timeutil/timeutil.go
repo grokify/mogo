6d6c8f05591825e00460c5c95701395a8297434d
John Wang <jwang@proofpoint.com>
2015-07-09 02:04:02 -0700

    Add timeutil.Dt14ForTime()

--- time/timeutil/timeutil.go
+++ time/timeutil/timeutil.go
@@ -22,16 +22,19 @@ func IsLessThan(timeLeft time.Time, timeRight time.Time) bool {
 	return false
 }
 
-func Dt8NowUtc() int32 {
-	tm := time.Now()
-	tm = tm.UTC()
-	return Dt8ForInts(tm.Year(), int(tm.Month()), tm.Day())
+func Dt8Now() int32 {
+	return Dt8ForTime(time.Now())
 }
 
 func Dt8ForInts(yyyy int, mm int, dd int) int32 {
 	sDt8 := fmt.Sprintf("%04d%02d%02d", yyyy, mm, dd)
 	iDt8, _ := strconv.ParseInt(sDt8, 10, 32)
 	return int32(iDt8)
+}
+
+func Dt8ForTime(t time.Time) int32 {
+	u := t.UTC()
+	return Dt8ForInts(u.Year(), int(u.Month()), u.Day())
 }
 
 func DurationForNowSubDt8(dt8 int32) (time.Duration, error) {
@@ -48,14 +51,17 @@ func TimeForDt8(dt8 int32) (time.Time, error) {
 	return time.Parse("20060102", strconv.FormatInt(int64(dt8), 10))
 }
 
-func Dt14NowUtc() int64 {
-	tm := time.Now()
-	tm = tm.UTC()
-	return Dt14ForInts(tm.Year(), int(tm.Month()), tm.Day(), tm.Hour(), tm.Minute(), tm.Second())
+func Dt14Now() int64 {
+	return Dt14ForTime(time.Now())
 }
 
 func Dt14ForInts(yyyy int, mm int, dd int, hr int, mn int, dy int) int64 {
 	sDt14 := fmt.Sprintf("%04d%02d%02d%02d%02d%02d", yyyy, mm, dd, hr, mn, dy)
 	iDt14, _ := strconv.ParseInt(sDt14, 10, 64)
 	return int64(iDt14)
+}
+
+func Dt14ForTime(t time.Time) int64 {
+	u := t.UTC()
+	return Dt14ForInts(u.Year(), int(u.Month()), u.Day(), u.Hour(), u.Minute(), u.Second())
 }
