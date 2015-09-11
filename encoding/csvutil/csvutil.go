7cd12bee22612453165db82ac22682ab8e1ccfe3
John Wang <johncwang@gmail.com>
2015-09-11 00:28:06 -0700

    add strip BOM support to csvutil

--- encoding/csvutil/csvutil.go
+++ encoding/csvutil/csvutil.go
@@ -5,12 +5,27 @@ import (
 	"os"
 )
 
-func NewReader(path string, comma rune) (*csv.Reader, *os.File, error) {
+/*
+
+For UTF-8 BOM, csv.Reader.Read() will return error = "line 1, column 1: bare \" in non-quoted-field"
+
+If you encounter this close the file and call again with stripBom = true
+
+*/
+
+func NewReader(path string, comma rune, stripBom bool) (*csv.Reader, *os.File, error) {
 	var myCsv *csv.Reader
 	var file *os.File
 	file, err := os.Open(path)
 	if err != nil {
 		return myCsv, file, err
+	}
+	if stripBom {
+		b3 := make([]byte, 3)
+		_, err := file.Read(b3)
+		if err != nil {
+			return myCsv, file, err
+		}
 	}
 	reader := csv.NewReader(file)
 	reader.Comma = comma
