2f0049164675e204395144091af6d65883e8209e
John Wang <johncwang@gmail.com>
2015-09-11 00:31:56 -0700

    update csvutil

--- encoding/csvutil/csvutil.go
+++ encoding/csvutil/csvutil.go
@@ -14,20 +14,20 @@ If you encounter this close the file and call again with stripBom = true
 */
 
 func NewReader(path string, comma rune, stripBom bool) (*csv.Reader, *os.File, error) {
-	var myCsv *csv.Reader
+	var csvReader *csv.Reader
 	var file *os.File
 	file, err := os.Open(path)
 	if err != nil {
-		return myCsv, file, err
+		return csvReader, file, err
 	}
 	if stripBom {
 		b3 := make([]byte, 3)
 		_, err := file.Read(b3)
 		if err != nil {
-			return myCsv, file, err
+			return csvReader, file, err
 		}
 	}
-	reader := csv.NewReader(file)
-	reader.Comma = comma
-	return reader, file, nil
+	csvReader = csv.NewReader(file)
+	csvReader.Comma = comma
+	return csvReader, file, nil
 }
