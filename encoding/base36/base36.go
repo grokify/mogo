95258045156c8a470607f5b8b144d4dc7ad69f19
John Wang <johncwang@gmail.com>
2015-11-07 22:47:50 -0800

    update godoc

--- encoding/base36/base36.go
+++ encoding/base36/base36.go
@@ -1,3 +1,4 @@
+// base36 supports Base36 encoding and decoding.
 package base36
 
 import (
@@ -9,6 +10,7 @@ import (
 	"github.com/grokify/gmp"
 )
 
+// Encode36String returns an encoded string given a byte array.
 func Encode36(ba []byte) string {
 	s16 := hex.EncodeToString(ba)
 	bi := gmp.NewInt(0)
@@ -16,14 +18,17 @@ func Encode36(ba []byte) string {
 	return bi.InBase(36)
 }
 
+// Encode36String returns an encoded string given a string.
 func Encode36String(s string) string {
 	return Encode36([]byte(s))
 }
 
+// Decode36 returns a decoded byte array given an encoded byte array.
 func Decode36(b36 []byte) ([]byte, error) {
 	return Decode36String(string(b36))
 }
 
+// Decode36String returns a decoded byte array given an encoded string.
 func Decode36String(s36 string) ([]byte, error) {
 	bi := gmp.NewInt(0)
 	bi.SetString(s36, 36)
@@ -31,6 +36,7 @@ func Decode36String(s36 string) ([]byte, error) {
 	return hex.DecodeString(s16)
 }
 
+// Md5Base36 returns a Base36 encoded MD5 hash of a string.
 func Md5Base36(s string) string {
 	h := md5.New()
 	io.WriteString(h, s)
