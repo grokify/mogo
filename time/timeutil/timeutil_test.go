7f19ee9eff55526c7ffd3b3327dd2b53d1e20fc4
John Wang <johncwang@gmail.com>
2016-01-18 00:32:39 -0800

    update tests

--- time/timeutil/timeutil_test.go
+++ time/timeutil/timeutil_test.go
@@ -2,6 +2,7 @@ package timeutil
 
 import (
 	"testing"
+	"time"
 )
 
 var dt6ForDt14Tests = []struct {
@@ -15,6 +16,24 @@ func TestDt6ForDt14(t *testing.T) {
 		got := Dt6ForDt14(tt.v)
 		if got != tt.want {
 			t.Errorf("Dt6ForDt14(%v): want %v, got %v", tt.v, tt.want, got)
+		}
+	}
+}
+
+var dt8ForStringTests = []struct {
+	v    string
+	want int32
+}{
+	{"2006-01-02T15:04:05Z", int32(20060102)}}
+
+func TestDt8ForString(t *testing.T) {
+	for _, tt := range dt8ForStringTests {
+		got, err := Dt8ForString(time.RFC3339, tt.v)
+		if err != nil {
+			t.Errorf("Dt8ForString(%v): want %v, error ", tt.v, tt.want, err)
+		}
+		if got != tt.want {
+			t.Errorf("Dt8ForString(%v): want %v, got %v", tt.v, tt.want, got)
 		}
 	}
 }
