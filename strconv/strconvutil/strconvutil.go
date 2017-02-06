355c9461428b750e14ac08af29b801d295d6ea43
John Wang <johncwang@gmail.com>
2017-02-05 22:32:18 -0800

    add strconvutil.Commify

---
+++ strconv/strconvutil/strconvutil.go
@@ -0,0 +1,27 @@
+package strconvutil
+
+import (
+	"strconv"
+)
+
+// Commify takes an int64 and adds comma for every thousand
+// Stack Overflow: http://stackoverflow.com/users/1705598/icza
+// URL: http://stackoverflow.com/questions/13020308/how-to-fmt-printf-an-integer-with-thousands-comma
+func Commify(n int64) string {
+	in := strconv.FormatInt(n, 10)
+	out := make([]byte, len(in)+(len(in)-2+int(in[0]/'0'))/3)
+	if in[0] == '-' {
+		in, out[0] = in[1:], '-'
+	}
+
+	for i, j, k := len(in)-1, len(out)-1, 0; ; i, j = i-1, j-1 {
+		out[j] = in[i]
+		if i == 0 {
+			return string(out)
+		}
+		if k++; k == 3 {
+			j, k = j-1, 0
+			out[j] = ','
+		}
+	}
+}
