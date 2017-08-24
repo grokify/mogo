29fce20e3f19ee8f74e0568ee8904c01ffc4864c
John Wang <johncwang@gmail.com>
2017-08-24 00:55:51 -0700

    add config/dotenv.go

---
+++ config/dotenv.go
@@ -0,0 +1,27 @@
+package config
+
+import (
+	"os"
+	"strings"
+
+	"github.com/joho/godotenv"
+)
+
+var EnvPathVars = []string{"ENV_PATH", "ENV_PATHS"}
+
+func LoadDotEnv() error {
+	envPathsSet := []string{}
+	for _, envPathVar := range EnvPathVars {
+		envPath := strings.TrimSpace(os.Getenv(envPathVar))
+		if len(envPath) > 0 {
+			envPaths := strings.Split(envPath, ",")
+			for _, envPath := range envPaths {
+				envPath = strings.TrimSpace(envPath)
+				if len(envPath) > 0 {
+					envPathsSet = append(envPathsSet, envPath)
+				}
+			}
+		}
+	}
+	return godotenv.Load(envPathsSet...)
+}
