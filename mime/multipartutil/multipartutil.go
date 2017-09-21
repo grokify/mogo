b4d99294b784378c4f1b44da11dc7567c35f2a8c
John Wang <johncwang@gmail.com>
2017-09-20 21:34:49 -0700

    add multipartutil.MultipartBuilder

---
+++ mime/multipartutil/multipartutil.go
@@ -0,0 +1,70 @@
+package multipartutil
+
+import (
+	"bytes"
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+	"io"
+	"mime/multipart"
+	"net/textproto"
+	"os"
+)
+
+// MultipartBuilder is a multipart helper.
+type MultipartBuilder struct {
+	Buffer *bytes.Buffer
+	Writer *multipart.Writer
+}
+
+// NewMultipartBuilder instantiates a new MultipartBuilder.
+func NewMultipartBuilder() MultipartBuilder {
+	builder := MultipartBuilder{}
+	var b bytes.Buffer
+	builder.Buffer = &b
+	builder.Writer = multipart.NewWriter(&b)
+	return builder
+}
+
+// WriteFieldAsJSON adds a JSON part.
+func (builder *MultipartBuilder) WriteFieldAsJSON(partName string, data interface{}, base64Encode bool) error {
+	jsonBytes, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
+
+	header := textproto.MIMEHeader{}
+	header.Add("Content-Disposition", fmt.Sprintf(`form-data; name="%v"`, partName))
+	header.Add("Content-Type", "application/json; charset=UTF-8")
+	if base64Encode {
+		header.Add("Content-Transfer-Encoding", "base64")
+	}
+
+	partWriter, err := builder.Writer.CreatePart(header)
+	if err != nil {
+		return err
+	}
+
+	if base64Encode {
+		str := base64.StdEncoding.EncodeToString(jsonBytes)
+		_, err = bytes.NewBuffer([]byte(str)).WriteTo(partWriter)
+	} else {
+		_, err = bytes.NewBuffer(jsonBytes).WriteTo(partWriter)
+	}
+	return err
+}
+
+// WriteFile adds a file part.
+func (builder *MultipartBuilder) WriteFile(partName, file string) error {
+	f, err := os.Open(file)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	fw, err := builder.Writer.CreateFormFile(partName, file)
+	if err != nil {
+		return err
+	}
+	_, err = io.Copy(fw, f)
+	return err
+}
