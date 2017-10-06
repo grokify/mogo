9d2c79856b4b7b69b7b29556c9f29715d1ab4a34
John Wang <johncwang@gmail.com>
2017-10-05 22:49:03 -0700

    add base10 encoding

---
+++ encoding/base10/base10.go
@@ -0,0 +1,16 @@
+// base10 supports Base10 encoding.
+package base10
+
+import (
+	"crypto/md5"
+	"math/big"
+)
+
+// Encode adapted from https://stackoverflow.com/questions/28128285/
+func Encode(bytes []byte) *big.Int {
+	bi := big.NewInt(0)
+	h := md5.New()
+	h.Write(bytes)
+	bi.SetBytes(h.Sum(nil))
+	return bi
+}
