a471f2cf7a43ee377414380a7c2ec7d946dd1bd8
John Wang <johncwang@gmail.com>
2017-11-05 01:47:46 -0700

    add tlsutil & pkcs12util

---
+++ crypto/tlsutil/tlsutil.go
@@ -0,0 +1,44 @@
+package tlsutil
+
+import (
+	"crypto/tls"
+	"crypto/x509"
+	"fmt"
+	"io/ioutil"
+)
+
+type TLSConfig struct {
+	Config *tls.Config
+}
+
+func NewTLSConfig() TLSConfig {
+	return TLSConfig{
+		Config: &tls.Config{
+			Certificates: []tls.Certificate{},
+			RootCAs:      x509.NewCertPool()}}
+}
+
+func (tc *TLSConfig) LoadX509KeyPair(cert_filepath, key_filepath string) error {
+	cert, err := tls.LoadX509KeyPair(cert_filepath, key_filepath)
+	if err != nil {
+		return err
+	}
+	tc.Config.Certificates = append(tc.Config.Certificates, cert)
+	return nil
+}
+
+func (tc *TLSConfig) LoadCACert(ca_cert_filepath string) error {
+	cert, err := ioutil.ReadFile(ca_cert_filepath)
+	if err != nil {
+		return err
+	}
+	ok := tc.Config.RootCAs.AppendCertsFromPEM(cert)
+	if !ok {
+		return fmt.Errorf("Cannot add Root CA cert %v", ca_cert_filepath)
+	}
+	return nil
+}
+
+func (tc *TLSConfig) Inflate() {
+	tc.Config.BuildNameToCertificate()
+}
--- encoding/base64/base64.go
+++ encoding/base64/base64.go
@@ -2,7 +2,6 @@ package base64
 
 import (
 	b64 "encoding/base64"
-	"errors"
 	"fmt"
 	"strings"
 )
@@ -11,9 +10,8 @@ import (
 // https://tools.ietf.org/html/rfc7617#section-2
 func RFC7617UserPass(userid, password string) (string, error) {
 	if strings.Index(userid, ":") > -1 {
-		return "", errors.New(
-			fmt.Sprintf(
-				"RFC7617 user-id cannot include a colon (':') [%v]", userid))
+		return "", fmt.Errorf(
+			"RFC7617 user-id cannot include a colon (':') [%v]", userid)
 	}
 	userpass := strings.Join([]string{userid, password}, ":")
 	return b64.StdEncoding.EncodeToString([]byte(userpass)), nil
