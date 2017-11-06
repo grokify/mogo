309015a979a0bdca9ac381fa28ef2439dad0fa32
John Wang <johncwang@gmail.com>
2017-11-05 18:56:37 -0800

    update sortutil

--- sort/sortutil/sortutil.go
+++ sort/sortutil/sortutil.go
@@ -6,28 +6,6 @@ import (
 	"strings"
 )
 
-func Int64Sorted(int64s []int64) []int64 {
-	ints := Int64sToInts(int64s)
-	sort.Ints(ints)
-	return IntsToInt64s(ints)
-}
-
-func Int64sToInts(int64s []int64) []int {
-	ints := []int{}
-	for _, x := range int64s {
-		ints = append(ints, int(x))
-	}
-	return ints
-}
-
-func IntsToInt64s(ints []int) []int64 {
-	int64s := []int64{}
-	for _, x := range ints {
-		int64s = append(int64s, int64(x))
-	}
-	return int64s
-}
-
 // For now, use only for slices < 100 in length for performance.
 // To do: more scalable implementation that uses sorting/searching.
 func InArrayStringCaseInsensitive(haystack []string, needle string) (string, error) {
