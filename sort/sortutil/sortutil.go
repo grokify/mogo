cf7b1cb544eaf7d4be430c5405f1730914b38815
John Wang <johncwang@gmail.com>
2017-11-05 18:55:18 -0800

    add sortutil.Int64s

--- sort/sortutil/sortutil.go
+++ sort/sortutil/sortutil.go
@@ -41,6 +41,18 @@ func InArrayStringCaseInsensitive(haystack []string, needle string) (string, err
 	return "", errors.New("String not found")
 }
 
+func Int64s(a []int64) { sort.Sort(Int64Slice(a)) }
+
+// Int64Slice attaches the methods of Interface to []iint64, sorting in increasing order.
+type Int64Slice []int64
+
+func (p Int64Slice) Len() int           { return len(p) }
+func (p Int64Slice) Less(i, j int) bool { return p[i] < p[j] }
+func (p Int64Slice) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
+
+// Sort is a convenience method.
+func (p Int64Slice) Sort() { sort.Sort(p) }
+
 // Uint16Slice attaches the methods of Interface to []uint16, sorting in increasing order.
 type Uint16Slice []uint16
 
