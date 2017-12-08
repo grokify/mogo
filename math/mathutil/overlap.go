e4a05ad997c1d064762b167ceff02bea90a97fc9
John Wang <johncwang@gmail.com>
2017-12-08 10:00:18 +0800

    add mathutil range overlap functions

--- math/mathutil/overlap.go
+++ math/mathutil/overlap.go
@@ -1,5 +1,13 @@
 package mathutil
 
+func IsEven(i int) bool {
+	return i%2 == 0
+}
+
+func IsOdd(i int) bool {
+	return !IsEven(i)
+}
+
 func IsOverlapSortedInt(x1, x2, y1, y2 int) bool {
 	return x1 <= y2 && y1 <= x2
 }
