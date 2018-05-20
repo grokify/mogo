873dfef50bcbcf30b6c4671eaeb15cd598887aaa
grokify <johncwang@gmail.com>
2018-05-20 09:49:06 -0700

    update comments

--- crypto/hash/argon2/argon2.go
+++ crypto/hash/argon2/argon2.go
@@ -6,6 +6,7 @@ import (
 )
 
 // HashSimple returns an argon2id hashed password encoded in Base36.
+// The base36 library always returns upper case.
 func HashSimpleBase36(input, salt []byte) string {
 	return base36.EncodeBytes(argon2lib.IDKey(input, salt, 1, 64*1024, 4, 32))
 }
