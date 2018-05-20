ee08344beafd6c18a33c5c2b06fcf4c2b4a1eb1b
grokify <johncwang@gmail.com>
2018-05-20 09:47:52 -0700

    add Argon2 password hash helper

---
+++ crypto/hash/argon2/argon2.go
@@ -0,0 +1,11 @@
+package argon2
+
+import (
+	"github.com/martinlindhe/base36"
+	argon2lib "golang.org/x/crypto/argon2"
+)
+
+// HashSimple returns an argon2id hashed password encoded in Base36.
+func HashSimpleBase36(input, salt []byte) string {
+	return base36.EncodeBytes(argon2lib.IDKey(input, salt, 1, 64*1024, 4, 32))
+}
---
+++ crypto/hash/argon2/argon2_test.go
@@ -0,0 +1,30 @@
+package argon2
+
+import (
+	"encoding/hex"
+	"testing"
+)
+
+var hashTests = []struct {
+	input string
+	salt  string
+	want  string
+}{
+	{"My Secret Password",
+		"6368616e676520746869732070617373776f726420746f206120736563726574",
+		"1XESZ8F6OGV200JDNXED8BAPU79UG5JA3KTJW7VMJCICX482S8"}}
+
+func TestHash(t *testing.T) {
+	for _, tt := range hashTests {
+		saltBytes, err := hex.DecodeString(tt.salt)
+		if err != nil {
+			t.Errorf("TestRoundTrip cannot decode key: %v", err)
+		}
+
+		got := HashSimpleBase36([]byte(tt.input), saltBytes)
+
+		if string(got) != tt.want {
+			t.Errorf("Argon2 Error: want [%v], got [%v]", tt.want, string(got))
+		}
+	}
+}
