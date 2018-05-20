a270c1c9e36ff20f511b94ed879d730643d05912
grokify <johncwang@gmail.com>
2018-05-19 21:36:52 -0700

    add tests for timeutil.DMYHM2

--- time/timeutil/format_test.go
+++ time/timeutil/format_test.go
@@ -3,7 +3,28 @@ package timeutil
 import (
 	"encoding/json"
 	"testing"
+	"time"
 )
+
+var dmyhm2ParseTests = []struct {
+	v    string
+	want string
+}{
+	{"02:01:06 15:04", "2006-01-02T15:04:00Z"},
+}
+
+// TestDMYHM2ParseTime ensures timeutil.DMYHM2 is parsed to GMT timezone.
+func TestDMYHM2ParseTime(t *testing.T) {
+	for _, tt := range dmyhm2ParseTests {
+		got, err := FromTo(tt.v, DMYHM2, time.RFC3339)
+		if err != nil {
+			t.Errorf("time.Parse(DMYHM2) Error: with %v, want %v, err %v", tt.v, tt.want, err)
+		}
+		if got != tt.want {
+			t.Errorf("time.Parse(\"%v\", DMYHM2) Mismatch: want %v, got %v", tt.v, tt.want, got)
+		}
+	}
+}
 
 var rfc3339YMDTimeTests = []struct {
 	v    string
