391b99e53e8f2b0f10a74e88ec2ca3055283a9e7
grokify <johncwang@gmail.com>
2018-05-21 09:18:30 -0700

    add HashSimpleBase32

--- crypto/hash/argon2/argon2.go
+++ crypto/hash/argon2/argon2.go
@@ -1,12 +1,28 @@
 package argon2
 
 import (
+	"encoding/base32"
+	"strings"
+
 	"github.com/martinlindhe/base36"
 	argon2lib "golang.org/x/crypto/argon2"
 )
 
+func HashSimple(input, salt []byte) []byte {
+	return argon2lib.IDKey(input, salt, 1, 64*1024, 4, 32)
+}
+
 // HashSimple returns an argon2id hashed password encoded in Base36.
 // The base36 library always returns upper case.
 func HashSimpleBase36(input, salt []byte) string {
-	return base36.EncodeBytes(argon2lib.IDKey(input, salt, 1, 64*1024, 4, 32))
+	return base36.EncodeBytes(HashSimple(input, salt))
+}
+
+// HashSimple returns an argon2id hashed password encoded in Base36.
+// The base36 library always returns upper case.
+func HashSimpleBase32(input, salt []byte, trim bool) string {
+	if trim {
+		return strings.Trim(base32.StdEncoding.EncodeToString(HashSimple(input, salt)), "=")
+	}
+	return base32.StdEncoding.EncodeToString(HashSimple(input, salt))
 }
