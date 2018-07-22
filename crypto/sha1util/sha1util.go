dc8c23c78190c7dbecbc81bfbb0e4361c3d5f3be
grokify <johncwang@gmail.com>
2018-07-21 18:24:53 -0700

    add sha1util.HashFile

---
+++ crypto/sha1util/sha1util.go
@@ -0,0 +1,23 @@
+package sha1util
+
+import (
+	"crypto/sha1"
+	"fmt"
+	"io"
+	"os"
+)
+
+func HashFile(file string) (string, error) {
+	f, err := os.Open(file)
+	if err != nil {
+		return "", err
+	}
+	defer f.Close()
+
+	h := sha1.New()
+	if _, err := io.Copy(h, f); err != nil {
+		return "", err
+	}
+
+	return fmt.Sprintf("%x", h.Sum(nil)), nil
+}
