395bd91a2493c57318b251290d1353a4a07479e6
grokify <johncwang@gmail.com>
2018-10-22 00:31:18 +0800

    update style

--- regexp/regexputil/regexputil.go
+++ regexp/regexputil/regexputil.go
@@ -5,7 +5,6 @@ import (
 )
 
 // McReplaceAllString is a single line MustCompile regexp for ReplaceAllString
-
 func McReplaceAllString(pattern string, s string, repl string) string {
 	return regexp.MustCompile(pattern).ReplaceAllString(s, repl)
 }
@@ -14,7 +13,6 @@ func McReplaceAllString(pattern string, s string, repl string) string {
 // Primary goals of this struct is to reduce MustCompile regular
 // expressions into a single function call and to store the compiled
 // regular expressions if desired
-
 type RegexpSet struct {
 	Regexps map[string]*regexp.Regexp
 }
@@ -54,7 +52,6 @@ func (set *RegexpSet) FindAllString(pattern string, s string, n int, useStore bo
 // FindAllStringSubmatch performs a regular expression find against the
 // supplied pattern and string. It will store the compiled regular expression
 // for later use.
-
 func (set *RegexpSet) FindAllStringSubmatch(pattern string, s string, n int, useStore bool, key string) [][]string {
 	rx := set.GetRegexp(pattern, useStore, key)
 	rs := rx.FindAllStringSubmatch(s, n)
