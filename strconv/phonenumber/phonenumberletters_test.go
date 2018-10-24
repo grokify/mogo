0994aeece4df1673c783f9c77ca4a8d12c66a6c7
grokify <johncwang@gmail.com>
2018-10-25 04:38:04 +0800

    update phonenumber.letterToNumberTests

--- strconv/phonenumber/phonenumberletters_test.go
+++ strconv/phonenumber/phonenumberletters_test.go
@@ -4,11 +4,17 @@ import (
 	"testing"
 )
 
+// numbers = "2ABC3DEF4GHI5JKL6MNO7PQRS8TUV9WXYZ"
+
 var letterToNumberTests = []struct {
 	v    string
 	want int
 }{
-	{"H", 4},
+	{"A", 2}, {"B", 2}, {"C", 2}, {"D", 3}, {"E", 3},
+	{"F", 3}, {"G", 4}, {"H", 4}, {"I", 4}, {"J", 5},
+	{"K", 5}, {"L", 5}, {"M", 6}, {"N", 6}, {"O", 6},
+	{"P", 7}, {"Q", 7}, {"R", 7}, {"S", 7}, {"T", 8},
+	{"U", 8}, {"V", 8}, {"W", 9}, {"X", 9}, {"Y", 9}, {"Z", 9},
 }
 
 func TestLetterToNumber(t *testing.T) {
