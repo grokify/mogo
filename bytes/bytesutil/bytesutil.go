66d1eef526b396896a053ffafce77da8e0a5f9d0
grokify <johncwang@gmail.com>
2019-03-09 22:25:28 -0800

    extract constant

--- bytes/bytesutil/bytesutil.go
+++ bytes/bytesutil/bytesutil.go
@@ -4,6 +4,8 @@ import (
 	"bytes"
 )
 
+const UTF8BOM = "\xef\xbb\xbf"
+
 func TrimUTF8BOM(data []byte) []byte {
-	return bytes.TrimPrefix(data, []byte("\xef\xbb\xbf"))
+	return bytes.TrimPrefix(data, []byte(UTF8BOM))
 }
