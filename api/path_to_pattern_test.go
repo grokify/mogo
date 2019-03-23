6b605f5566a843d8e8ca89c8be19a524344961c5
grokify <johncwang@gmail.com>
2019-03-23 05:17:57 -0700

    add api.URLTransformer tests

--- api/path_to_pattern_test.go
+++ api/path_to_pattern_test.go
@@ -9,15 +9,18 @@ var urlToPatternTests = []struct {
 	want string
 }{
 	{"/pets", "/pets"},
+	{"/pets?q=Fido", "/pets"},
 	{"/pets/dog", "/pets/{petId}"},
-	{"/pets/dog/vacinations", "/pets/{petId}/vacinations"}}
+	{"/pets/dog/vacinations", "/pets/{petId}/vacinations"},
+	{"/pets/dog/vacinations/123", "/pets/{petId}/vacinations/{vacinationId}"}}
 
 func TestURLToPattern(t *testing.T) {
 	ut := NewURLTransformer()
 	ut.LoadPaths([]string{
 		"/pets",
 		"/pets/{petId}",
-		"/pets/{petId}/vacinations"})
+		"/pets/{petId}/vacinations",
+		"/pets/{petId}/vacinations/{vacinationId}"})
 
 	for _, tt := range urlToPatternTests {
 		got := ut.URLActualToPattern(tt.v)
