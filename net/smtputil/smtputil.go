bd376a20a6b5d83fa6c7b1cbc7e62aa0d5e05e61
John Wang <johncwang@gmail.com>
2019-07-20 13:41:15 -0700

    add smtputil.GmailAddressPlusTime

---
+++ net/smtputil/smtputil.go
@@ -0,0 +1,24 @@
+package smtputil
+
+import (
+	"errors"
+	"fmt"
+	"strings"
+	"time"
+
+	"github.com/grokify/gotilla/time/timeutil"
+)
+
+// GmailHost is the Google mail smtp hostname.
+const GmailHost = "gmail.com"
+
+// GmailUserPlusTime creates a Gmail SMTP address appending
+// a time in "DT14" time format.
+func GmailAddressPlusTime(smtpUser string) (string, error) {
+	smtpUser = strings.TrimSpace(smtpUser)
+	if len(smtpUser) == 0 {
+		return "", errors.New("No SMTP User address provided.")
+	}
+	t := time.Now()
+	return fmt.Sprintf(`%s+%s@%s`, smtpUser, t.Format(timeutil.DT14), GmailHost), nil
+}
