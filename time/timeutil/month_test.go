87e1bb11b4a632c01b6bf5d4f14855e9bfda5e6d
John Wang <johncwang@gmail.com>
2019-09-02 19:44:00 -0700

    add timeutil.YearMonthBase36 and timeutil.YearMonthBase36Time

--- time/timeutil/month_test.go
+++ time/timeutil/month_test.go
@@ -26,3 +26,26 @@ func TestDayofmonthToEnglish(t *testing.T) {
 		}
 	}
 }
+
+var yearMonthBase36Tests = []struct {
+	year  uint64
+	month uint64
+	want  string
+}{
+	{uint64(1), uint64(1), "002T"},
+	{uint64(500), uint64(3), "12KZ"},
+	{uint64(1000), uint64(6), "255Y"},
+	{uint64(2010), uint64(8), "4B3K"},
+	{uint64(2019), uint64(8), "4BSK"},
+	{uint64(2038), uint64(1), "4D95"},
+	{uint64(9999), uint64(12), "LFJC"},
+}
+
+func TestYearMonthBase36(t *testing.T) {
+	for _, tt := range yearMonthBase36Tests {
+		got := YearMonthBase36(tt.year, tt.month)
+		if got != tt.want {
+			t.Errorf("YearMonthBase36(%v, %v): want %v, got %v", tt.year, tt.month, tt.want, got)
+		}
+	}
+}
