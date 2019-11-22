712cb89e0a472d856facaa2471b1c338ecc155fc
John Wang <johncwang@gmail.com>
2019-11-22 01:03:38 -0800

    add HTTPMethod type

--- net/httputilmore/http_headers.go
+++ net/httputilmore/http_headers.go
@@ -1,5 +1,11 @@
 package httputilmore
 
+import (
+	"fmt"
+	"net/http"
+	"strings"
+)
+
 // Constants ensuring that header names are correctly spelled and consistently cased.
 const (
 	HeaderAccept                       = "Accept"
@@ -32,3 +38,69 @@ const (
 	ContentTypeTextXmlUtf8             = "text/xml; charset=utf-8"
 	SchemeHTTPS                        = "https"
 )
+
+// HTTPMethod is a type of HTTP Methods. Of note, do not
+// rely on the integer value but use for definitions.
+type HTTPMethod int
+
+const (
+	MethodConnect HTTPMethod = iota
+	MethodDelete
+	MethodGet
+	MethodHead
+	MethodOptions
+	MethodPatch
+	MethodPost
+	MethodPut
+	MethodTrace
+)
+
+var methods = [...]string{
+	http.MethodConnect,
+	http.MethodDelete,
+	http.MethodGet,
+	http.MethodHead,
+	http.MethodOptions,
+	http.MethodPatch,
+	http.MethodPost,
+	http.MethodPut,
+	http.MethodTrace,
+}
+
+// String returns the English name of the day ("Sunday", "Monday", ...).
+func (method HTTPMethod) String() string {
+	if MethodConnect <= method && method <= MethodTrace {
+		return methods[method]
+	}
+	buf := make([]byte, 20)
+	n := fmtInt(buf, uint64(method))
+	return "%!HTTPMethod(" + string(buf[n:]) + ")"
+}
+
+// fmtInt formats v into the tail of buf.
+// It returns the index where the output begins.
+func fmtInt(buf []byte, v uint64) int {
+	w := len(buf)
+	if v == 0 {
+		w--
+		buf[w] = '0'
+	} else {
+		for v > 0 {
+			w--
+			buf[w] = byte(v%10) + '0'
+			v /= 10
+		}
+	}
+	return w
+}
+
+// ParseHTTPMethod returns a HTTPMethod type for a string.
+func ParseHTTPMethod(method string) (HTTPMethod, error) {
+	method = strings.ToUpper(strings.TrimSpace(method))
+	for i, try := range methods {
+		if method == try {
+			return HTTPMethod(i), nil
+		}
+	}
+	return MethodConnect, fmt.Errorf("E_NO_METHOD_FOR [%v]", method)
+}
