ccf62fe730ea07d1d022d71c43b9f2b8dcfdf215
John Wang <johncwang@gmail.com>
2019-12-11 02:35:01 -0800

    add stringsutil.JoinCustom()

--- type/stringsutil/slice.go
+++ type/stringsutil/slice.go
@@ -199,6 +199,65 @@ func SliceJoinQuotedMaxLength(slice []string, begQuote, endQuote, sep string, ma
 	return words
 }
 
+type JoinCustomConfig struct {
+	QuoteBegin string
+	QuoteEnd   string
+	Separator  string
+	MaxLength  int
+	TrimSpace  bool
+	SkipEmpty  bool
+}
+
+func JoinCustom(slice []string, cfg JoinCustomConfig) []string {
+	lines := []string{}
+	curWords := []string{}
+	curLength := 0
+	for _, word := range slice {
+		if cfg.TrimSpace {
+			word = strings.TrimSpace(word)
+		}
+		if cfg.SkipEmpty && len(word) == 0 {
+			continue
+		}
+		if cfg.MaxLength > 0 {
+			if curLength+len(cfg.QuoteBegin+word+cfg.QuoteEnd+cfg.Separator) > cfg.MaxLength {
+				lines = append(lines, strings.Join(curWords, cfg.Separator))
+				curWords = []string{}
+				curLength = 0
+			} else {
+				curWords = append(curWords, cfg.QuoteBegin+word+cfg.QuoteEnd)
+				curLength += len(cfg.QuoteBegin + word + cfg.QuoteEnd + cfg.Separator)
+			}
+		} else {
+			curWords = append(curWords, cfg.QuoteBegin+word+cfg.QuoteEnd)
+		}
+	}
+	if len(curWords) > 0 {
+		lines = append(lines, strings.Join(curWords, cfg.Separator))
+	}
+	return lines
+}
+
+func SliceJoinQuotedMaxLengthTrimSpaceSkipEmpty(slice []string, begQuote, endQuote, sep string, maxLength int) []string {
+	words := []string{}
+	curWords := []string{}
+	curLength := 0
+	for _, word := range slice {
+		if curLength+len(begQuote+word+endQuote+sep) > maxLength {
+			words = append(words, strings.Join(curWords, sep))
+			curWords = []string{}
+			curLength = 0
+		} else {
+			curWords = append(curWords, begQuote+word+endQuote)
+			curLength += len(begQuote + word + endQuote + sep)
+		}
+	}
+	if len(curWords) > 0 {
+		words = append(words, strings.Join(curWords, sep))
+	}
+	return words
+}
+
 func SliceJoinQuoted(slice []string, begQuote, endQuote, sep string) string {
 	words := []string{}
 	for _, word := range slice {
