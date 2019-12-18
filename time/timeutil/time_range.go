b00e9bf7ae10466bebcfebf554d0474c448c08d6
John Wang <johncwang@gmail.com>
2019-12-19 04:50:03 +0800

    simplify code

--- time/timeutil/time_range.go
+++ time/timeutil/time_range.go
@@ -7,14 +7,10 @@ import "time"
 // exclusive. Exclusive ranges are the default.
 func InRange(rangeStart, rangeEnd, needle time.Time, incStart, incEnd bool) bool {
 	rangeStart, rangeEnd = MinMax(rangeStart, rangeEnd)
-	if incStart && needle.Before(rangeStart) {
-		return false
-	} else if needle.Before(rangeStart) || needle.Equal(rangeStart) {
-		return false
-	}
-	if incEnd && needle.After(rangeEnd) {
-		return false
-	} else if needle.After(rangeEnd) || needle.Equal(rangeEnd) {
+	if (incStart && needle.Before(rangeStart)) ||
+		(needle.Before(rangeStart) || needle.Equal(rangeStart)) ||
+		(incEnd && needle.After(rangeEnd)) ||
+		(needle.After(rangeEnd) || needle.Equal(rangeEnd)) {
 		return false
 	}
 	return true
