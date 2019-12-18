061b03f267c5120852a8cc31dc914f7bdeafdf4c
John Wang <johncwang@gmail.com>
2019-12-19 04:32:29 +0800

    add comment

--- time/timeutil/time_range.go
+++ time/timeutil/time_range.go
@@ -2,6 +2,9 @@ package timeutil
 
 import "time"
 
+// InRange checks to see if a time is within a provided time range
+// with options whether the begin and end ranges are inclusive or
+// exclusive. Exclusive ranges are the default.
 func InRange(rangeStart, rangeEnd, needle time.Time, incStart, incEnd bool) bool {
 	rangeStart, rangeEnd = MinMax(rangeStart, rangeEnd)
 	if incStart && needle.Before(rangeStart) {
