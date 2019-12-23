79a35b5605e394c8fb757facb347ce89429ed851
John Wang <johncwang@gmail.com>
2019-12-24 07:33:06 +0800

    add mathutil.MinMaxInt32()

--- math/mathutil/mathutil.go
+++ math/mathutil/mathutil.go
@@ -7,3 +7,10 @@ func DivMod(numerator, denominator int64) (quotient, remainder int64) {
 }
 
 // from https://stackoverflow.com/questions/43945675/division-with-returning-quotient-and-remainder
+
+func MinMaxInt32(a, b int32) (int32, int32) {
+	if b < a {
+		return b, a
+	}
+	return a, b
+}
