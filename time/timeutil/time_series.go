5c39b5cf77df77aa6d2f44a024f88c4652c0c324
John Wang <johncwang@gmail.com>
2020-02-24 22:58:58 -0800

    update time series interfaces

--- time/timeutil/time_series.go
+++ time/timeutil/time_series.go
@@ -5,17 +5,17 @@ import (
 	"time"
 )
 
-// SliceTimeSeries builds a time series based on supplied interval.
-func SliceTimeSeries(times []time.Time, interval Interval) []time.Time {
+// TimeSeriesSlice builds a time series based on supplied interval.
+func TimeSeriesSlice(interval Interval, times []time.Time) []time.Time {
 	if len(times) == 0 {
 		return times
 	}
 	min, max := SliceMinMax(times)
-	return MinMaxTimeSeries(min, max, interval)
+	return TimeSeriesMinMax(interval, min, max)
 }
 
-// MinMaxTimeSeries builds a time series based on supplied interval.
-func MinMaxTimeSeries(min, max time.Time, interval Interval) []time.Time {
+// TimeSeriesMinMax builds a time series based on supplied interval.
+func TimeSeriesMinMax(interval Interval, min, max time.Time) []time.Time {
 	min, max = MinMax(min, max)
 	series := []time.Time{}
 	switch interval {
