81bbec06c64df17e5a016c7a395c35b87aa4bd07
John Wang <johncwang@gmail.com>
2020-04-04 02:36:03 -0700

    add imageutil/examples

---
+++ image/examples/resize_and_overlay/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/grokify/gotilla/image/imageutil"
+	"github.com/pkg/errors"
+)
+
+// https://www.golangprograms.com/how-to-add-watermark-or-merge-two-image.html
+
+func main() {
+	fileTop := "_top.png"
+	fileBkg := "_background.png"
+	fileOut := "_output.png"
+
+	imgTop, err := imageutil.ReadImage(fileTop)
+	if err != nil {
+		log.Fatal(errors.Wrap(err, fileTop))
+	}
+	imgBkg, err := imageutil.ReadImage(fileBkg)
+	if err != nil {
+		log.Fatal(errors.Wrap(err, fileBkg))
+	}
+
+	imgTop = imageutil.AddBackgroundWhite(imgTop)
+	imgTop = imageutil.Resize(120, 0, imgTop, imageutil.BestScaler())
+	imgOut := imageutil.OverlayCenterYLeftAlign(imgBkg, imgTop)
+	err = imageutil.WriteFilePNG(fileOut, imgOut)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("DONE")
+}
