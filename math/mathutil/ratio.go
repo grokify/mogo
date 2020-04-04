f90c37b7c6fe44f00de1c115c72ba0ffc24a1600
John Wang <johncwang@gmail.com>
2020-04-04 00:27:43 -0700

    update comment

--- math/mathutil/ratio.go
+++ math/mathutil/ratio.go
@@ -1,6 +1,7 @@
 package mathutil
 
 // RatioInt generates the missing value in a ratio calculation.
+// Missing number should be in second set of coordinates.
 func RatioInt(x1, y1, x2, y2 int) (int, int) {
 	if x2 > 0 && y2 > 0 {
 		return x2, y2
