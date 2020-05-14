70763f4a2addbb0a8ebec75f60d3d1c6605398c0
John Wang <johncwang@gmail.com>
2020-05-14 02:39:48 -0700

    update mathutil.MinMaxInt32, add MinMaxUint

--- math/mathutil/mathutil.go
+++ math/mathutil/mathutil.go
@@ -3,18 +3,50 @@ package mathutil
 // DivideInt64 performs integer division, returning
 // a quotient and remainder.
 func DivideInt64(dividend, divisor int64) (quotient, remainder int64) {
+	// from https://stackoverflow.com/questions/43945675/division-with-returning-quotient-and-remainder
 	quotient = dividend / divisor // integer division, decimals are truncated
 	remainder = dividend % divisor
 	return
 }
 
-// from https://stackoverflow.com/questions/43945675/division-with-returning-quotient-and-remainder
-
-// MinMaxInt32 returns min/max value given two
+// MinMaxInt32 returns min/max value given a slice of
 // input values.
-func MinMaxInt32(a, b int32) (int32, int32) {
-	if b < a {
-		return b, a
+func MinMaxInt32(vals ...int32) (int32, int32) {
+	min := int32(0)
+	max := int32(0)
+	for i, val := range vals {
+		if i == 0 {
+			min = val
+			max = val
+		} else {
+			if val < min {
+				min = val
+			}
+			if val > max {
+				max = val
+			}
+		}
 	}
-	return a, b
+	return min, max
+}
+
+// MinMaxUint returns min/max value given a slice of
+// input values.
+func MinMaxUint(vals ...uint) (uint, uint) {
+	min := uint(0)
+	max := uint(0)
+	for i, val := range vals {
+		if i == 0 {
+			min = val
+			max = val
+		} else {
+			if val < min {
+				min = val
+			}
+			if val > max {
+				max = val
+			}
+		}
+	}
+	return min, max
 }
--- math/mathutil/mathutil_test.go
+++ math/mathutil/mathutil_test.go
@@ -1,6 +1,8 @@
 package mathutil
 
 import (
+	"strconv"
+	"strings"
 	"testing"
 )
 
@@ -21,6 +23,45 @@ func TestDivideInt64(t *testing.T) {
 				tt.dividend, tt.divisor,
 				tt.quotient, tt.remainder,
 				quotient, remainder)
+		}
+	}
+}
+
+var minMaxTests = []struct {
+	vals []int
+	min  int
+	max  int
+}{
+	{[]int{4, 7, 8}, 4, 8},
+	{[]int{-4, -7, 8}, -7, 8},
+}
+
+func JoinIntsString(ints []int, sep string) string {
+	strs := []string{}
+	for _, val := range ints {
+		strs = append(strs, strconv.Itoa(val))
+	}
+	return strings.Join(strs, sep)
+}
+
+func TestMinMax(t *testing.T) {
+	for _, tt := range minMaxTests {
+		strs := JoinIntsString(tt.vals, ",")
+		// Test Int32
+		int32s := IntsToInt32s(tt.vals)
+		min32, max32 := MinMaxInt32(int32s...)
+		if min32 != int32(tt.min) || max32 != int32(tt.max) {
+			t.Errorf("mathutil.MinMaxInt32(%s) Mismatch: want [%d,%d], got [%d,%d]",
+				strs, tt.min, tt.max, min32, max32)
+		}
+		if min32 >= 0 {
+			// Test Uint
+			uints := IntsToUints(tt.vals)
+			minUint, maxUint := MinMaxUint(uints...)
+			if minUint != uint(tt.min) || maxUint != uint(tt.max) {
+				t.Errorf("mathutil.MinMaxUint(%s) Mismatch: want [%d,%d], got [%d,%d]",
+					strs, tt.min, tt.max, minUint, maxUint)
+			}
 		}
 	}
 }
