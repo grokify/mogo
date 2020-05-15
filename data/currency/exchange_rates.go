82467b97975c63fd5e69f6d91cb281ca0bfe2d95
John Wang <johncwang@gmail.com>
2020-05-14 22:08:39 -0700

    add currency.ExchangeRateSimple example

--- data/currency/exchange_rates.go
+++ data/currency/exchange_rates.go
@@ -17,3 +17,14 @@ func (xr *ExchangeRateSimple) ConvertToBase(in float64, cur string) (float64, er
 	}
 	return in, fmt.Errorf("E_EXCHANGE_RATE_CANNOT_FIND [%s]", cur)
 }
+
+func ExampleExchangeRates() ExchangeRateSimple {
+	return ExchangeRateSimple{
+		BaseCurrency: "USD",
+		CurrencyRates: map[string]float64{
+			"AUD": 0.637368,
+			"CAD": 0.709545,
+			"EUR": 1.08641,
+			"GBP": 1.24626,
+			"USD": 1.0}}
+}
