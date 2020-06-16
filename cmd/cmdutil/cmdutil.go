64da51221b6bfb6ba111f90bc646706056b67ba6
John Wang <johncwang@gmail.com>
2020-06-16 09:39:58 -0700

    enhance: cmdutil.ExecSimple add os.FileMode

--- cmd/cmdutil/cmdutil.go
+++ cmd/cmdutil/cmdutil.go
@@ -3,12 +3,13 @@ package cmdutil
 import (
 	"bytes"
 	"io/ioutil"
+	"os"
 	"os/exec"
 	"strings"
 )
 
 // ExecSimple provides a simple interface to execute a system command.
-func ExecSimple(command, stdoutFile, stderrFile string) (bytes.Buffer, bytes.Buffer, error) {
+func ExecSimple(command, stdoutFile, stderrFile string, perm os.FileMode) (bytes.Buffer, bytes.Buffer, error) {
 	var stdout bytes.Buffer
 	var stderr bytes.Buffer
 	parts := strings.Split(command, " ")
@@ -22,13 +23,13 @@ func ExecSimple(command, stdoutFile, stderrFile string) (bytes.Buffer, bytes.Buf
 	stdoutFile = strings.TrimSpace(stdoutFile)
 	stderrFile = strings.TrimSpace(stderrFile)
 	if len(stdoutFile) > 0 {
-		err := ioutil.WriteFile(stdoutFile, stdout.Bytes(), 0644)
+		err := ioutil.WriteFile(stdoutFile, stdout.Bytes(), perm)
 		if err != nil {
 			return stdout, stderr, err
 		}
 	}
 	if len(stderrFile) > 0 {
-		err := ioutil.WriteFile(stderrFile, stdout.Bytes(), 0644)
+		err := ioutil.WriteFile(stderrFile, stdout.Bytes(), perm)
 		if err != nil {
 			return stdout, stderr, err
 		}
