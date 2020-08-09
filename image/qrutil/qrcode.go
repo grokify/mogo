89765c8eb7a54e03e84d47be2f4a9e125b94f5c6
John Wang <johncwang@gmail.com>
2020-08-09 12:36:18 -0700

    feat: qrutil to build QRCodes

---
+++ image/qrutil/qrcode.go
@@ -0,0 +1,43 @@
+// qrutil provides wrapper functions for https://github.com/skip2/go-qrcode s.
+package qrutil
+
+import "github.com/skip2/go-qrcode"
+
+type Opts struct {
+	QRCode    qrcode.QRCode
+	Filename  string
+	Filesize  int
+	FileWrite bool
+}
+
+// New creates a new `*qrcode.QRCode` using `qrcode.QRCode`
+// as the request parameter.
+func New(opts qrcode.QRCode) (*qrcode.QRCode, error) {
+	q, err := qrcode.New(opts.Content, opts.Level)
+	if err != nil {
+		return nil, err
+	}
+
+	if opts.BackgroundColor != opts.ForegroundColor {
+		q.BackgroundColor = opts.BackgroundColor
+		q.ForegroundColor = opts.ForegroundColor
+	}
+
+	if opts.DisableBorder {
+		q.DisableBorder = true
+	}
+	return q, nil
+}
+
+func Create(opts Opts) (*qrcode.QRCode, error) {
+	q, err := New(opts.QRCode)
+	if err != nil {
+		return nil, err
+	}
+
+	if opts.FileWrite {
+		err = q.WriteFile(opts.Filesize, opts.Filename)
+	}
+
+	return q, err
+}
