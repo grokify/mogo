8303de42fffecff44b9f37b6fe27ff0f1bdaaa83
John Wang <johncwang@gmail.com>
2020-08-09 03:38:33 -0700

    refactor: simplify code

--- image/imageutil/combine.go
+++ image/imageutil/combine.go
@@ -10,8 +10,8 @@ func OverlayCenterYLeftAlign(imgBg, imgOver image.Image) image.Image {
 	output := image.NewRGBA(imgBg.Bounds())
 	draw.Draw(output, imgBg.Bounds(), imgBg, image.ZP, draw.Src)
 
-	_, h1 := ImageWidthHeight(imgBg)
-	_, h2 := ImageWidthHeight(imgOver)
+	h1 := imgBg.Bounds().Dy()
+	h2 := imgOver.Bounds().Dy()
 	offset := image.Pt(0, (h1-h2)/2)
 
 	draw.Draw(output, imgOver.Bounds().Add(offset), imgOver, image.ZP, draw.Src)
