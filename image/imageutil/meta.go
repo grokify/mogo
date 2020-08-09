6569b7669424b57822cf326c825b1da1915abf33
John Wang <johncwang@gmail.com>
2020-08-09 03:37:47 -0700

    refactor: simplify code

--- image/imageutil/meta.go
+++ image/imageutil/meta.go
@@ -1,15 +1,18 @@
 package imageutil
 
-import "image"
+import (
+	"image"
+)
 
 func ImageAspect(img image.Image) float64 {
-	return Aspect(ImageWidthHeight(img))
+	return Aspect(img.Bounds().Dx(), img.Bounds().Dy())
 }
 
 func Aspect(width, height int) float64 {
 	return float64(width) / float64(height)
 }
 
+/*
 func ImageWidthHeight(img image.Image) (int, int) {
 	return RectangleWidthHeight(img.Bounds())
 }
@@ -18,3 +21,4 @@ func RectangleWidthHeight(rect image.Rectangle) (int, int) {
 	return rect.Max.X - rect.Min.X,
 		rect.Max.Y - rect.Min.Y
 }
+*/
