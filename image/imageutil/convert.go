394f54dd53bb8f0dcd0f306a96b879e2bf2e6ab1
John Wang <johncwang@gmail.com>
2020-08-23 14:15:21 -0700

    feat: imageutil: add ImageWithSet; add ImageWithSetToRGBA(); add ImageAnyToRGBA()

--- image/imageutil/convert.go
+++ image/imageutil/convert.go
@@ -1,8 +1,11 @@
 package imageutil
 
 import (
+	"fmt"
 	"image"
+	"image/color"
 
+	"github.com/grokify/gotilla/reflect/reflectutil"
 	"golang.org/x/image/draw"
 )
 
@@ -11,12 +14,27 @@ func ImageToRGBA(img image.Image) *image.RGBA {
 	switch img := img.(type) {
 	case *image.NRGBA:
 		return NRGBAtoRGBA(img)
+	case *image.Paletted:
+		return ImageWithSetToRGBA(img)
 	case *image.RGBA:
 		return img
 	case *image.YCbCr:
 		return YCbCrToRGBA(img)
 	}
-	panic("Format not supported.")
+	panic(fmt.Sprintf("Format not supported [%v].", reflectutil.TypeName(img)))
+}
+
+type ImageWithSet interface {
+	// ColorModel returns the Image's color model.
+	ColorModel() color.Model
+	// Bounds returns the domain for which At can return non-zero color.
+	// The bounds do not necessarily contain the point (0, 0).
+	Bounds() image.Rectangle
+	// At returns the color of the pixel at (x, y).
+	// At(Bounds().Min.X, Bounds().Min.Y) returns the upper-left pixel of the grid.
+	// At(Bounds().Max.X-1, Bounds().Max.Y-1) returns the lower-right one.
+	At(x, y int) color.Color
+	Set(x, y int, c color.Color)
 }
 
 func NRGBAtoRGBA(imgNRGBA *image.NRGBA) *image.RGBA {
@@ -28,6 +46,25 @@ func NRGBAtoRGBA(imgNRGBA *image.NRGBA) *image.RGBA {
 		}
 	}
 	return imgRGBA
+}
+
+func ImageWithSetToRGBA(src ImageWithSet) *image.RGBA {
+	rect := src.Bounds()
+	imgRGBA := image.NewRGBA(rect)
+	for x := rect.Min.X; x <= rect.Max.X; x++ {
+		for y := rect.Min.Y; y <= rect.Max.Y; y++ {
+			imgRGBA.Set(x, y, src.At(x, y))
+		}
+	}
+	return imgRGBA
+}
+
+func ImageAnyToRGBA(src image.Image) *image.RGBA {
+	// https://stackoverflow.com/questions/31463756/convert-image-image-to-image-nrgba
+	b := src.Bounds()
+	img := image.NewRGBA(image.Rect(0, 0, b.Dx(), b.Dy()))
+	draw.Draw(img, img.Bounds(), src, b.Min, draw.Src)
+	return img
 }
 
 func YCbCrToRGBA(src *image.YCbCr) *image.RGBA {
