1a1a121675e7c502f5ffbc4939b789d4f8536704
John Wang <johncwang@gmail.com>
2020-09-24 04:11:53 -0700

    feat: imageutil/combine: add MergeXSameY+Http, MergeYSameX+Http

--- image/imageutil/combine.go
+++ image/imageutil/combine.go
@@ -14,6 +14,68 @@ func OverlayCenterYLeftAlign(imgBg, imgOver image.Image) image.Image {
 	h2 := imgOver.Bounds().Dy()
 	offset := image.Pt(0, (h1-h2)/2)
 
-	draw.Draw(output, imgOver.Bounds().Add(offset), imgOver, image.ZP, draw.Src)
+	draw.Draw(output, imgOver.Bounds().Add(offset), imgOver, image.Point{}, draw.Src)
 	return output
+}
+
+func MergeXSameY(img1, img2 image.Image, larger bool) image.Image {
+	img1, img2 = ResizeSameY(img1, img2, larger)
+	output := image.NewRGBA(
+		image.Rect(0, 0,
+			img1.Bounds().Dx()+img2.Bounds().Dx(),
+			img1.Bounds().Dy()))
+	draw.Draw(output, img1.Bounds(), img1, image.Point{}, draw.Src)
+	img2Offset := image.Pt(img1.Bounds().Dx(), 0)
+	draw.Draw(output, img2.Bounds().Add(img2Offset), img2, image.Point{}, draw.Src)
+	return output
+}
+
+func MergeXSameYHttp(url1, url2 string, larger bool) (image.Image, error) {
+	img1, _, err := ReadImageHttp(url1)
+	if err != nil {
+		return img1, err
+	}
+	img2, _, err := ReadImageHttp(url2)
+	if err != nil {
+		return img2, err
+	}
+	return MergeXSameY(img1, img2, true), nil
+}
+
+func MergeYSameX(img1, img2 image.Image, larger bool) image.Image {
+	img1, img2 = ResizeSameX(img1, img2, larger)
+	output := image.NewRGBA(
+		image.Rect(0, 0,
+			img1.Bounds().Dx(),
+			img1.Bounds().Dy()+img2.Bounds().Dy()))
+	draw.Draw(output, img1.Bounds(), img1, image.Point{}, draw.Src)
+	img2Offset := image.Pt(0, img1.Bounds().Dy())
+	draw.Draw(output, img2.Bounds().Add(img2Offset), img2, image.Point{}, draw.Src)
+	return output
+}
+
+func MergeYSameXHttp(url1, url2 string, larger bool) (image.Image, error) {
+	img1, _, err := ReadImageHttp(url1)
+	if err != nil {
+		return img1, err
+	}
+	img2, _, err := ReadImageHttp(url2)
+	if err != nil {
+		return img2, err
+	}
+	return MergeYSameX(img1, img2, true), nil
+}
+
+func Merge4Http(url1, url2, url3, url4 string, larger bool) (image.Image, error) {
+	img12, err := MergeXSameYHttp(url1, url2, larger)
+	if err != nil {
+		return img12, err
+	}
+
+	img34, err := MergeXSameYHttp(url3, url4, larger)
+	if err != nil {
+		return img34, err
+	}
+
+	return MergeYSameX(img12, img34, larger), nil
 }
