931996c733560a01dad502cd7957a502ebd4bea3
John Wang <johncwang@gmail.com>
2020-09-28 06:47:37 -0700

    feat: number: add IntSliceDedupe()

--- type/number/number.go
+++ type/number/number.go
@@ -1,9 +1,29 @@
 package number
 
+import "sort"
+
 func SliceInt64ToFloat64(src []int64) []float64 {
 	out := []float64{}
 	for _, in := range src {
 		out = append(out, float64(in))
 	}
 	return out
+}
+
+func IntSliceDedupe(elems []int, sortElems bool) []int {
+	if len(elems) == 0 {
+		return elems
+	}
+	deduped := []int{}
+	mapInts := map[int]int{}
+	for _, el := range elems {
+		if _, ok := mapInts[el]; !ok {
+			deduped = append(deduped, el)
+			mapInts[el] = 1
+		}
+	}
+	if sortElems {
+		sort.Ints(deduped)
+	}
+	return deduped
 }
