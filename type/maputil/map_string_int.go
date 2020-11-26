c32942604d224d5ccb14d2ea765afa98edbdd71e
John Wang <johncwang@gmail.com>
2020-11-25 16:59:48 -0800

    enhance: maputil: `MapStringInt` and `MapStringMapStringInt`

--- type/maputil/map_string_int.go
+++ type/maputil/map_string_int.go
@@ -4,15 +4,23 @@ import (
 	"sort"
 )
 
+// MapStringInt represents a `map[string]int`
 type MapStringInt map[string]int
 
-func (msi MapStringInt) AddKey(key string, val int) {
+// Set sets the value of `val` to `key`.
+func (msi MapStringInt) Set(key string, val int) {
+	msi[key] = val
+}
+
+// Add adds the value of `val` to `key`.
+func (msi MapStringInt) Add(key string, val int) {
 	if _, ok := msi[key]; !ok {
 		msi[key] = 0
 	}
 	msi[key] += val
 }
 
+// Keys returns a string slice of the map's keys.
 func (msi MapStringInt) Keys(sortKeys bool) []string {
 	keys := []string{}
 	for key := range msi {
@@ -24,13 +32,16 @@ func (msi MapStringInt) Keys(sortKeys bool) []string {
 	return keys
 }
 
-func (msi MapStringInt) KeyValue(key string, defaultValue int) int {
+// MustGet returns a value or a default.
+func (msi MapStringInt) MustGet(key string, defaultValue int) int {
 	if val, ok := msi[key]; ok {
 		return val
 	}
 	return defaultValue
 }
 
+// MinMaxValues returns the minium and maximum values
+// of the `map[string]int`.
 func (msi MapStringInt) MinMaxValues() (int, int) {
 	min := 0
 	max := 0
--- type/maputil/map_string_map_string_int.go
+++ type/maputil/map_string_map_string_int.go
@@ -7,9 +7,11 @@ import (
 	"github.com/grokify/gotilla/type/stringsutil"
 )
 
-type MapStrMapStrInt map[string]map[string]int
+// MapStringMapStringInt represents a `map[string]map[string]int`
+type MapStringMapStringInt map[string]map[string]int
 
-func (msmsi MapStrMapStrInt) Add(str1, str2 string, val int) {
+// Set supports setting values to `MapStringMapStringInt` easily.
+func (msmsi MapStringMapStringInt) Set(str1, str2 string, val int) {
 	if _, ok := msmsi[str1]; !ok {
 		msmsi[str1] = map[string]int{}
 	}
@@ -18,7 +20,20 @@ func (msmsi MapStrMapStrInt) Add(str1, str2 string, val int) {
 	}
 	msmsi[str1][str2] = val
 }
-func (msmsi MapStrMapStrInt) Flatten(prefix, sep string, dedupe, sortResults bool) []string {
+
+// Add supports adding values to `MapStringMapStringInt` easily.
+func (msmsi MapStringMapStringInt) Add(str1, str2 string, val int) {
+	if _, ok := msmsi[str1]; !ok {
+		msmsi[str1] = map[string]int{}
+	}
+	if _, ok := msmsi[str1][str2]; !ok {
+		msmsi[str1][str2] = 0
+	}
+	msmsi[str1][str2] += val
+}
+
+// Flatten returns the values in the `MapStringMapStringInt` in an string slice.
+func (msmsi MapStringMapStringInt) Flatten(prefix, sep string, dedupe, sortResults bool) []string {
 	outvals := []string{}
 	for str1, map1 := range msmsi {
 		for str2 := range map1 {
