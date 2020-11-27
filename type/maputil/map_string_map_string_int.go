888aea5499ffd1da21c7f5a1300fc84c61092b46
John Wang <johncwang@gmail.com>
2020-11-27 04:03:33 -0800

    feat: maputil/MapStringMapStringInt: add count and filter by value functions

--- type/maputil/map_string_map_string_int.go
+++ type/maputil/map_string_map_string_int.go
@@ -32,11 +32,65 @@ func (msmsi MapStringMapStringInt) Add(str1, str2 string, val int) {
 	msmsi[str1][str2] += val
 }
 
-// Flatten returns the values in the `MapStringMapStringInt` in an string slice.
-func (msmsi MapStringMapStringInt) Flatten(prefix, sep string, dedupe, sortResults bool) []string {
-	outvals := []string{}
+// Counts returns key counts regardless of value.
+func (msmsi MapStringMapStringInt) Counts(sep string) (int, int) {
+	count1Info := map[string]int{}
+	count2Info := map[string]int{}
+	if len(sep) == 0 {
+		sep = "~~~"
+	}
 	for str1, map1 := range msmsi {
 		for str2 := range map1 {
+			count1Info[str1] = 1
+			count2Info[str1+sep+str2] = 1
+		}
+	}
+	return len(count1Info), len(count2Info)
+}
+
+// CountsWithVal returns key counts with the desired value.
+func (msmsi MapStringMapStringInt) CountsWithVal(wantVal int, sep string) (int, int) {
+	count1Info := map[string]int{}
+	count2Info := map[string]int{}
+	if len(sep) == 0 {
+		sep = "~~~"
+	}
+	for str1, map1 := range msmsi {
+		for str2, val := range map1 {
+			if wantVal == val {
+				count1Info[str1] = 1
+				count2Info[str1+sep+str2] = 1
+			}
+		}
+	}
+	return len(count1Info), len(count2Info)
+}
+
+// MapStringMapStringIntFuncExactMatch returns a match function
+// that matches an exact vaule.
+func MapStringMapStringIntFuncExactMatch(valWant int) func(string, string, int) bool {
+	return func(str1, str2 string, val int) bool {
+		if valWant == val {
+			return true
+		}
+		return false
+	}
+}
+
+// MapStringMapStringIntFuncIncludeAll returns match function
+// that matches all values.
+func MapStringMapStringIntFuncIncludeAll(str1, str2 string, val int) bool {
+	return true
+}
+
+// Flatten returns the values in the `MapStringMapStringInt` in an string slice.
+func (msmsi MapStringMapStringInt) Flatten(prefix, sep string, fnInclude func(string, string, int) bool, dedupe, sortResults bool) []string {
+	outvals := []string{}
+	for str1, map1 := range msmsi {
+		for str2, val := range map1 {
+			if !fnInclude(str1, str2, val) {
+				continue
+			}
 			parts := []string{}
 			if len(prefix) > 0 {
 				parts = append(parts, prefix)
