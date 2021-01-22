123e396256340ddf9b42e1b72c6379bf7602a6da
John Wang <johncwang@gmail.com>
2021-01-21 19:10:35 -0800

    feat: httpsimple: Test handlers

--- net/http/httpsimple/simpleserver.go
+++ net/http/httpsimple/simpleserver.go
@@ -1,14 +1,17 @@
 package httpsimple
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 	"strconv"
 	"strings"
+	"time"
 
 	"github.com/apex/gateway"
 	"github.com/buaazp/fasthttprouter"
+	"github.com/grokify/simplego/net/anyhttp"
 	"github.com/valyala/fasthttp"
 )
 
@@ -56,3 +59,27 @@ func Serve(svc SimpleServer) {
 }
 
 func portAddress(port int) string { return ":" + strconv.Itoa(port) }
+
+type TestResponse struct {
+	Command    string    `json:"command"`
+	RequestURL string    `json:"requestURL"`
+	Time       time.Time `json:"time"`
+}
+
+func HandleTestFastHTTP(ctx *fasthttp.RequestCtx) {
+	HandleTestAnyEngine(anyhttp.NewResReqFastHttp(ctx))
+}
+
+func HandleTestNetHTTP(res http.ResponseWriter, req *http.Request) {
+	HandleTestAnyEngine(anyhttp.NewResReqNetHttp(res, req))
+}
+
+func HandleTestAnyEngine(aRes anyhttp.Response, aReq anyhttp.Request) {
+	test := TestResponse{
+		Command:    "pong",
+		RequestURL: string(aReq.RequestURI()),
+		Time:       time.Now().UTC()}
+	bytes, _ := json.Marshal(test)
+	aRes.SetStatusCode(http.StatusOK)
+	aRes.SetBodyBytes(bytes)
+}
