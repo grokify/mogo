4f9a1f5d18595ce6a2662354fb90aa4bf7ca7d18
John Wang <johncwang@gmail.com>
2021-04-03 17:45:43 -0700

    update: fmtutil: remove published variables

--- fmt/fmtutil/fmtutil.go
+++ fmt/fmtutil/fmtutil.go
@@ -3,7 +3,6 @@ package fmtutil
 
 import (
 	"encoding/json"
-	"expvar"
 	"fmt"
 	"io"
 	"io/ioutil"
@@ -18,10 +17,12 @@ var (
 )
 
 // init uses expvar to export package variables to simplify method signatures.
+/*
 func init() {
 	expvar.Publish("JSONPrefix", expvar.NewString(""))
 	expvar.Publish("JSONIndent", expvar.NewString("  "))
 }
+*/
 
 // PrintJSON pretty prints anything using a default indentation
 func PrintJSON(in interface{}) error {
