d6ba3fcb804817c380a3efd912b05d1128205713
John Wang <johncwang@gmail.com>
2021-05-07 22:20:17 -0700

    feat: logutil: add `Severity` and `SeverityInclude()`

---
+++ log/logutil/severity.go
@@ -0,0 +1,71 @@
+package severity
+
+import "fmt"
+
+type Severity string
+
+const (
+	SeverityDisabled      Severity = "disabled"
+	SeverityEmergency     Severity = "emergency"
+	SeverityAlert         Severity = "alert"
+	SeverityCritical      Severity = "critical"
+	SeverityError         Severity = "error"
+	SeverityWarning       Severity = "warning"
+	SeverityNotice        Severity = "notice"
+	SeverityInformational Severity = "informational"
+	SeverityDebug         Severity = "debug"
+)
+
+var severities = map[Severity]int{
+	SeverityDisabled:      -1,
+	SeverityEmergency:     0,
+	SeverityAlert:         2,
+	SeverityCritical:      3,
+	SeverityError:         4,
+	SeverityWarning:       5,
+	SeverityNotice:        6,
+	SeverityInformational: 7,
+	SeverityDebug:         8,
+}
+
+func Parse(sev string) Severity {
+	switch sev {
+	case string(SeverityDisabled):
+		return SeverityDisabled
+	case string(SeverityEmergency):
+		return SeverityEmergency
+	case string(SeverityAlert):
+		return SeverityAlert
+	case string(SeverityCritical):
+		return SeverityCritical
+	case string(SeverityError):
+		return SeverityError
+	case string(SeverityWarning):
+		return SeverityWarning
+	case string(SeverityNotice):
+		return SeverityNotice
+	case string(SeverityInformational):
+		return SeverityInformational
+	case string(SeverityDebug):
+		return SeverityDebug
+	}
+	return SeverityDisabled
+}
+
+func SeverityInclude(reportLevel, itemLevel Severity) (bool, error) {
+	reportLevelInt, ok := severities[reportLevel]
+	if !ok {
+		return false, fmt.Errorf("reportLevel [%s] not supported", reportLevel)
+	}
+	if reportLevelInt < 1 {
+		return false, nil
+	}
+	itemLevelInt, ok := severities[itemLevel]
+	if !ok {
+		return false, fmt.Errorf("itemLevel [%s] not supported", itemLevel)
+	}
+	if itemLevelInt <= reportLevelInt {
+		return true, nil
+	}
+	return false, nil
+}
---
+++ log/logutil/severity_test.go
@@ -0,0 +1,31 @@
+package severity
+
+import (
+	"testing"
+)
+
+var severityTests = []struct {
+	execSeverity Severity
+	itemSeverity Severity
+	include      bool
+	errorIsNil   bool
+}{
+	{SeverityError, SeverityWarning, false, true},
+	{SeverityError, SeverityCritical, true, true},
+	{SeverityDebug, SeverityWarning, true, true},
+	{SeverityDebug, SeverityCritical, true, true},
+	{SeverityCritical, SeverityError, false, true},
+	{"foo", "bar", false, false},
+}
+
+func TestSeverity(t *testing.T) {
+	for _, tt := range severityTests {
+		tryIncl, err := SeverityInclude(tt.execSeverity, tt.itemSeverity)
+		if err != nil && tt.errorIsNil {
+			t.Errorf("logutil.SeverityInclude(\"%s\",\"%s\") error [%v]", tt.execSeverity, tt.itemSeverity, err.Error())
+		}
+		if tryIncl != tt.include {
+			t.Errorf("logutil.SeverityInclude(\"%s\",\"%s\") want [%v] got [%v]", tt.execSeverity, tt.itemSeverity, tt.include, tryIncl)
+		}
+	}
+}
