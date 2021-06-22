d5c5d9eef4ad2f3ab81f45f8ad55a6673a468ee4
John Wang <johncwang@gmail.com>
2021-06-22 04:13:28 -0700

    enhance: text: add `Texts.DisplayForSlug()`

--- text/text.go
+++ text/text.go
@@ -1,10 +1,34 @@
 package text
 
+import (
+	"fmt"
+)
+
 type Text struct {
 	Display string
 	Slug    string
 }
 
+type Texts []Text
+
+func (texts Texts) DisplayForSlug(slug string) (string, error) {
+	for _, try := range texts {
+		if slug == try.Slug {
+			return try.Display, nil
+		}
+	}
+	return "", fmt.Errorf("slug not found [%s]", slug)
+}
+
+func (texts Texts) DisplayTexts() []string {
+	displays := []string{}
+	for _, txt := range texts {
+		displays = append(displays, txt.Display)
+	}
+	return displays
+}
+
+/*
 type TextSet struct {
 	Texts []Text
 }
@@ -16,3 +40,4 @@ func (ts *TextSet) DisplayTexts() []string {
 	}
 	return displays
 }
+*/
