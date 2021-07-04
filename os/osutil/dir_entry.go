e95b1637fa88b59c775d7d32ebe6d2aa77d1f6f0
John Wang <johncwang@gmail.com>
2021-07-04 03:09:07 -0700

    enhance: osutil: add `DirEntrySlice.Names()`

--- os/osutil/dir_entry.go
+++ os/osutil/dir_entry.go
@@ -11,3 +11,11 @@ func (p DirEntrySlice) Len() int           { return len(p) }
 func (p DirEntrySlice) Less(i, j int) bool { return p[i].Name() < p[j].Name() }
 func (p DirEntrySlice) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
 func (p DirEntrySlice) Sort()              { sort.Sort(p) }
+
+func (p DirEntrySlice) Names() []string {
+	names := []string{}
+	for _, item := range p {
+		names = append(names, item.Name())
+	}
+	return names
+}
