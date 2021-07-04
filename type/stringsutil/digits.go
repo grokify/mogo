cdb4cfe2d8fcd6a3f4c0f90f6ca96e3f805cfa8e
John Wang <johncwang@gmail.com>
2021-07-04 02:27:07 -0700

    feat: type/stringsutil: add `IsInteger()`

--- type/stringsutil/digits.go
+++ type/stringsutil/digits.go
@@ -9,3 +9,9 @@ var rxNonDigit = regexp.MustCompile(`[^0-9]`)
 func DigitsOnly(input string) string {
 	return rxNonDigit.ReplaceAllString(input, "")
 }
+
+var rxDigits = regexp.MustCompile(`^[0-9]+$`)
+
+func IsInteger(input string) bool {
+	return rxDigits.MatchString(input)
+}
