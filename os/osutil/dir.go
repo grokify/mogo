448c6d97fc34458330ab7c44a610b320300bcf56
John Wang <johncwang@gmail.com>
2021-07-05 03:35:09 -0700

    feat: osutil: add `ReadDirRxSubmatch()`, `ReadDirRxSubmatchGreatestEntries()`, `ReadDirRxSubmatchGreatestCapture()`

--- os/osutil/dir.go
+++ os/osutil/dir.go
@@ -1,10 +1,13 @@
 package osutil
 
 import (
+	"errors"
 	"fmt"
 	"os"
 	"regexp"
 	"sort"
+
+	"github.com/grokify/simplego/type/maputil"
 )
 
 func ReadDirMore(dir string, rx *regexp.Regexp, inclDirs, inclFiles, inclEmptyFiles bool) ([]os.DirEntry, error) {
@@ -60,4 +63,77 @@ func ReadSubdirMax(dir string, rx *regexp.Regexp) (os.DirEntry, error) {
 	}
 	sort.Sort(DirEntrySlice(sdirs))
 	return sdirs[len(sdirs)-1], nil
+}
+
+// ReadDirRxSubmatch takes a directory, regular expression and boolean to indicate
+// whether to include zero size files and returns the greatest of a single match in the
+// regular expression.
+func ReadDirRxSubmatch(dir string, rx *regexp.Regexp, subMatchIdx uint, inclDirs, inclFiles, inclEmptyFiles bool) (map[string][]os.DirEntry, error) {
+	entryMap := map[string][]os.DirEntry{}
+
+	entries, err := os.ReadDir(dir)
+	if err != nil {
+		return entryMap, err
+	}
+
+	for _, entry := range entries {
+		if entry.IsDir() {
+			if !inclDirs {
+				continue
+			}
+		} else if !inclFiles {
+			continue
+		}
+		if !inclEmptyFiles && !entry.IsDir() {
+			fi, err := entry.Info()
+			if err != nil {
+				return entryMap, err
+			}
+			if fi.Size() <= 0 {
+				continue
+			}
+		}
+		m := rx.FindStringSubmatch(entry.Name())
+		if len(m) <= 0 {
+			continue
+		}
+		if int(subMatchIdx) >= len(m) {
+			return entryMap, fmt.Errorf("index too large for matches. matches [%d] idx [%d]", len(m), subMatchIdx)
+		}
+		submatch := m[int(subMatchIdx)]
+		if _, ok := entryMap[submatch]; !ok {
+			entryMap[submatch] = []os.DirEntry{}
+		}
+		entryMap[submatch] = append(entryMap[submatch], entry)
+	}
+
+	return entryMap, nil
+}
+
+func ReadDirRxSubmatchGreatestEntries(dir string, rx *regexp.Regexp, subMatchIdx uint, inclDirs, inclFiles, inclEmptyFiles bool) ([]os.DirEntry, error) {
+	entryMap, err := ReadDirRxSubmatch(dir, rx, subMatchIdx, inclDirs, inclFiles, inclEmptyFiles)
+	if err != nil {
+		return []os.DirEntry{}, err
+	}
+	if len(entryMap) == 0 {
+		return []os.DirEntry{}, nil
+	}
+	keysSorted := maputil.StringKeysSorted(entryMap)
+	greatest := keysSorted[len(keysSorted)-1]
+	return entryMap[greatest], nil
+}
+
+// ReadDirRxSubmatchGreatestCapture takes a directory, regular expression and returns the greatest of a single submatch in the
+// regular expression.
+func ReadDirRxSubmatchGreatestCapture(dir string, rx *regexp.Regexp, subMatchIdx uint, inclDirs, inclFiles, inclEmptyFiles bool) (string, error) {
+	entryMap, err := ReadDirRxSubmatch(dir, rx, subMatchIdx, inclDirs, inclFiles, inclEmptyFiles)
+	if err != nil {
+		return "", err
+	}
+	if len(entryMap) == 0 {
+		return "", errors.New("no match for ReadDirRxSubmatchGreatestMatch")
+	}
+	keysSorted := maputil.StringKeysSorted(entryMap)
+	greatest := keysSorted[len(keysSorted)-1]
+	return greatest, nil
 }
