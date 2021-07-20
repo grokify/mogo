ce5cad415de3194091aa917d3f81471a083558b1
John Wang <johncwang@gmail.com>
2021-07-20 05:33:24 -0700

    feat: tokenizer: add `TokenAttribute()`

--- html/tokenizer/attribute.go
+++ html/tokenizer/attribute.go
@@ -27,3 +27,18 @@ func (attrs Attributes) GetOne(attributeKey string) (html.Attribute, error) {
 	}
 	return matches[0], nil
 }
+
+func TokenAttribute(token html.Token, attrName string, attrLower bool) (string, error) {
+	attrNameMatch := attrName
+	if attrLower {
+		attrNameMatch = strings.ToLower(attrNameMatch)
+	}
+	for _, attr := range token.Attr {
+		if attrLower && strings.ToLower(attr.Key) == attrNameMatch {
+			return attr.Val, nil
+		} else if !attrLower && attr.Key == attrNameMatch {
+			return attr.Val, nil
+		}
+	}
+	return "", fmt.Errorf("attribute not found [%s] matchLower [%v]", attrName, attrLower)
+}
