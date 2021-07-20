a9cf69308a49f19abbcffe2f8fda1d4d1190472b
John Wang <johncwang@gmail.com>
2021-07-20 05:33:53 -0700

    feat: tokenizer: `ParseLink()`

--- html/tokenizer/token.go
+++ html/tokenizer/token.go
@@ -1,6 +1,8 @@
 package tokenizer
 
 import (
+	"errors"
+	"fmt"
 	"strings"
 
 	"golang.org/x/net/html"
@@ -14,4 +16,15 @@ func (tokens Tokens) String() string {
 		toks = append(toks, tok.String())
 	}
 	return strings.Join(toks, "")
+}
+
+func ParseLink(tokens ...html.Token) (string, string, error) {
+	if len(tokens) < 3 {
+		return "", "", fmt.Errorf("less than 3 tokens [%d]", len(tokens))
+	}
+	href, err := TokenAttribute(tokens[0], "href", true)
+	if err != nil {
+		return href, "", errors.New("href not found")
+	}
+	return href, Tokens(tokens[1 : len(tokens)-1]).String(), nil
 }
