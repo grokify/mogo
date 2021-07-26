f4551d0437ffb7d0ad2aa4d0cd3a4f237f19a622
John Wang <johncwang@gmail.com>
2021-07-25 20:02:29 -0700

    style: io/fsutil: update formatting

--- io/fsutil/direntry.go
+++ io/fsutil/direntry.go
@@ -11,8 +11,7 @@ func (de DirEntries) Names(dir string) []string {
 	filenames := []string{}
 	for _, entry := range de {
 		if len(dir) > 0 {
-			filenames = append(filenames,
-				filepath.Join(dir, entry.Name()))
+			filenames = append(filenames, filepath.Join(dir, entry.Name()))
 		} else {
 			filenames = append(filenames, entry.Name())
 		}
