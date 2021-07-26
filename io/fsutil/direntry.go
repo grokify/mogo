e12970ed0c304a5f90a773da7c3ed9419ad79cdc
John Wang <johncwang@gmail.com>
2021-07-25 20:01:13 -0700

    feat: io/fsutil: add `DirEntries{}`, `DirEntries.Names()`

---
+++ io/fsutil/direntry.go
@@ -0,0 +1,21 @@
+package fsutil
+
+import (
+	"io/fs"
+	"path/filepath"
+)
+
+type DirEntries []fs.DirEntry
+
+func (de DirEntries) Names(dir string) []string {
+	filenames := []string{}
+	for _, entry := range de {
+		if len(dir) > 0 {
+			filenames = append(filenames,
+				filepath.Join(dir, entry.Name()))
+		} else {
+			filenames = append(filenames, entry.Name())
+		}
+	}
+	return filenames
+}
