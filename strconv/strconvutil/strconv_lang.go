7251b2e097882e6e2ed49a4dcaa85331a537bcb3
John Wang <johncwang@gmail.com>
2021-08-13 02:40:07 -0700

    feat: strconvutil: add `AtoiLang()`

---
+++ strconv/strconvutil/strconv_lang.go
@@ -0,0 +1,63 @@
+package strconvutil
+
+import (
+	"fmt"
+	"regexp"
+	"strconv"
+	"strings"
+
+	"github.com/grokify/simplego/text/languageutil"
+)
+
+var rxComma = regexp.MustCompile(`,`)
+
+// AtoiLang provides language parsing to handle
+// thousands separators.
+// Number formats: https://docs.oracle.com/cd/E19455-01/806-0169/overview-9/index.html
+func AtoiLang(lang, s string) (int, error) {
+	s = strings.TrimSpace(s)
+	sepThousands, err := ThousandsSeparator(lang)
+	if err != nil {
+		return 0, err
+	}
+	if len(sepThousands) > 0 {
+		s = strings.Replace(s, sepThousands, "", -1)
+	}
+	return strconv.Atoi(s)
+}
+
+func DecimalSeparator(lang string) (string, error) {
+	lang = strings.ToLower(strings.TrimSpace(lang))
+	if lang == languageutil.English ||
+		lang == languageutil.Thai {
+		return ".", nil
+	} else if lang == languageutil.Italian ||
+		lang == languageutil.Norwegian ||
+		lang == languageutil.Spanish {
+		return ",", nil
+	} else if lang == languageutil.Danish ||
+		lang == languageutil.Finnish ||
+		lang == languageutil.French ||
+		lang == languageutil.German {
+		return ",", nil
+	}
+	return "", fmt.Errorf("language not found [%s]", lang)
+}
+
+func ThousandsSeparator(lang string) (string, error) {
+	lang = strings.ToLower(strings.TrimSpace(lang))
+	if lang == languageutil.English ||
+		lang == languageutil.Thai {
+		return ",", nil
+	} else if lang == languageutil.Italian ||
+		lang == languageutil.Norwegian ||
+		lang == languageutil.Spanish {
+		return ".", nil
+	} else if lang == languageutil.Danish ||
+		lang == languageutil.Finnish ||
+		lang == languageutil.French ||
+		lang == languageutil.German {
+		return " ", nil
+	}
+	return "", fmt.Errorf("language not found [%s]", lang)
+}
---
+++ strconv/strconvutil/strconv_lang_test.go
@@ -0,0 +1,37 @@
+package strconvutil
+
+import (
+	"testing"
+
+	"github.com/grokify/simplego/text/languageutil"
+)
+
+var strconvLangTests = []struct {
+	lang   string
+	strval string
+	intval int
+}{
+	{languageutil.English, "1,234,567,890", 1234567890},
+	{languageutil.Thai, "1,234,567,890", 1234567890},
+	{languageutil.Danish, "1 234 567 890", 1234567890},
+	{languageutil.Finnish, "1 234 567 890", 1234567890},
+	{languageutil.German, "1 234 567 890", 1234567890},
+	{languageutil.French, "1 234 567 890", 1234567890},
+	{languageutil.Italian, "1.234.567.890", 1234567890},
+	{languageutil.Norwegian, "1.234.567.890", 1234567890},
+	{languageutil.Spanish, "1.234.567.890", 1234567890},
+}
+
+func TestAtoiLang(t *testing.T) {
+	for _, tt := range strconvLangTests {
+		tryInt, err := AtoiLang(tt.lang, tt.strval)
+		if err != nil {
+			t.Errorf("strconvutil.AtoiLang(\"%s\", \"%s\") Error: [%s]",
+				tt.lang, tt.strval, err.Error())
+		}
+		if err == nil && tryInt != tt.intval {
+			t.Errorf("strconvutil.AtoiLang(\"%s\", \"%s\") Error: want [%d], got [%d]",
+				tt.lang, tt.strval, tt.intval, tryInt)
+		}
+	}
+}
