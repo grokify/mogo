78ec9f454dfab23b8535ecb900cad62caa1262db
John Wang <johncwang@gmail.com>
2021-08-15 21:51:26 -0700

    enhance: osutil: combine `IsFile()` and `IsFileWithSizeGtZero()`

--- os/osutil/filepath.go
+++ os/osutil/filepath.go
@@ -13,10 +13,15 @@ func IsDir(name string) (bool, error) {
 	return true, nil
 }
 
-func IsFile(name string) (bool, error) {
+// IsFile verifies a path exists, is a file and is not empty,
+// returning an error otherwise. An os file not exists check can be done
+// with os.IsNotExist(err) which acts on error from os.Stat()
+func IsFile(name string, sizeGtZero bool) (bool, error) {
 	if fi, err := os.Stat(name); err != nil {
 		return false, err
 	} else if !fi.Mode().IsRegular() {
+		return false, nil
+	} else if sizeGtZero && fi.Size() <= 0 {
 		return false, nil
 	}
 	return true, nil
@@ -30,22 +35,6 @@ func Exists(name string) (bool, error) {
 		return false, nil
 	} else if err != nil {
 		return false, err
-	}
-	return true, nil
-}
-
-// IsFileWithSizeGtZero verifies a path exists, is a file and is not empty,
-// returning an error otherwise. An os file not exists check can be done
-// with os.IsNotExist(err) which acts on error from os.Stat()
-func IsFileWithSizeGtZero(name string) (bool, error) {
-	if fi, err := os.Stat(name); err != nil {
-		return false, err
-	} else if !fi.Mode().IsRegular() {
-		return false, nil
-		// return fmt.Errorf("Filepath [%v] exists but is not a file.", name)
-	} else if fi.Size() <= 0 {
-		return false, nil
-		// return fmt.Errorf("Filepath [%v] exists but is empty with size [%v].", name, fi.Size())
 	}
 	return true, nil
 }
