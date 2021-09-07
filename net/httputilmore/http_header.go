0eab22915489ac7c59887e5b32a7a4e100d7756d
John Wang <johncwang@gmail.com>
2021-09-07 04:55:25 -0700

    refactor: httputilmore

--- net/httputilmore/http_header.go
+++ net/httputilmore/http_header.go
@@ -1,9 +1,7 @@
 package httputilmore
 
 import (
-	"fmt"
 	"net/http"
-	"strings"
 )
 
 // Constants ensuring that header names are correctly spelled and consistently cased.
@@ -47,97 +45,6 @@ const (
 	ContentTypeTextXmlUtf8             = "text/xml; charset=utf-8"
 	SchemeHTTPS                        = "https"
 )
-
-type HTTPMethod string
-
-const (
-	MethodConnect HTTPMethod = http.MethodConnect
-	MethodDelete             = http.MethodDelete
-	MethodGet                = http.MethodGet
-	MethodHead               = http.MethodHead
-	MethodOptions            = http.MethodOptions
-	MethodPatch              = http.MethodPatch
-	MethodPost               = http.MethodPost
-	MethodPut                = http.MethodPut
-	MethodTrace              = http.MethodTrace
-)
-
-// ParseHTTPMethod returns a HTTPMethod type for a string.
-func ParseHTTPMethod(method string) (HTTPMethod, error) {
-	method = strings.ToUpper(strings.TrimSpace(method))
-	switch method {
-	case http.MethodConnect:
-		return MethodConnect, nil
-	case http.MethodDelete:
-		return MethodDelete, nil
-	case http.MethodGet:
-		return MethodGet, nil
-	case http.MethodHead:
-		return MethodHead, nil
-	case http.MethodOptions:
-		return MethodOptions, nil
-	case http.MethodPatch:
-		return MethodPatch, nil
-	case http.MethodPost:
-		return MethodPost, nil
-	case http.MethodPut:
-		return MethodPut, nil
-	case http.MethodTrace:
-		return MethodTrace, nil
-	}
-	return MethodConnect, fmt.Errorf("E_NO_METHOD_FOR [%v]", method)
-}
-
-/*
-// HTTPMethod is a type of HTTP Methods. Of note, do not
-// rely on the integer value but use for definitions.
-type HTTPMethod int
-
-const (
-	MethodConnect HTTPMethod = iota
-	MethodDelete
-	MethodGet
-	MethodHead
-	MethodOptions
-	MethodPatch
-	MethodPost
-	MethodPut
-	MethodTrace
-)
-
-var methods = [...]string{
-	http.MethodConnect,
-	http.MethodDelete,
-	http.MethodGet,
-	http.MethodHead,
-	http.MethodOptions,
-	http.MethodPatch,
-	http.MethodPost,
-	http.MethodPut,
-	http.MethodTrace,
-}
-
-// String returns the English name of the HTTPMethod.
-func (method HTTPMethod) String() string {
-	if MethodConnect <= method && method <= MethodTrace {
-		return methods[method]
-	}
-	buf := make([]byte, 20)
-	n := fmtInt(buf, uint64(method))
-	return "%!HTTPMethod(" + string(buf[n:]) + ")"
-}
-
-// ParseHTTPMethod returns a HTTPMethod type for a string.
-func ParseHTTPMethod(method string) (HTTPMethod, error) {
-	method = strings.ToUpper(strings.TrimSpace(method))
-	for i, try := range methods {
-		if method == try {
-			return HTTPMethod(i), nil
-		}
-	}
-	return MethodConnect, fmt.Errorf("E_NO_METHOD_FOR [%v]", method)
-}
-*/
 
 // fmtInt formats v into the tail of buf.
 // It returns the index where the output begins.
---
+++ net/httputilmore/http_method.go
@@ -0,0 +1,61 @@
+package httputilmore
+
+import (
+	"fmt"
+	"net/http"
+	"strings"
+)
+
+type HTTPMethod string
+
+const (
+	MethodConnect HTTPMethod = http.MethodConnect
+	MethodDelete             = http.MethodDelete
+	MethodGet                = http.MethodGet
+	MethodHead               = http.MethodHead
+	MethodOptions            = http.MethodOptions
+	MethodPatch              = http.MethodPatch
+	MethodPost               = http.MethodPost
+	MethodPut                = http.MethodPut
+	MethodTrace              = http.MethodTrace
+)
+
+// ParseHTTPMethod returns a HTTPMethod type for a string.
+func ParseHTTPMethod(method string) (HTTPMethod, error) {
+	methodCanonical := strings.ToUpper(strings.TrimSpace(method))
+	switch methodCanonical {
+	case http.MethodConnect:
+		return MethodConnect, nil
+	case http.MethodDelete:
+		return MethodDelete, nil
+	case http.MethodGet:
+		return MethodGet, nil
+	case http.MethodHead:
+		return MethodHead, nil
+	case http.MethodOptions:
+		return MethodOptions, nil
+	case http.MethodPatch:
+		return MethodPatch, nil
+	case http.MethodPost:
+		return MethodPost, nil
+	case http.MethodPut:
+		return MethodPut, nil
+	case http.MethodTrace:
+		return MethodTrace, nil
+	}
+	return MethodConnect, fmt.Errorf("cannot parse method [%v]", method)
+}
+
+func MethodsMap() map[string]int {
+	return map[string]int{
+		http.MethodConnect: 1,
+		http.MethodDelete:  1,
+		http.MethodGet:     1,
+		http.MethodHead:    1,
+		http.MethodOptions: 1,
+		http.MethodPatch:   1,
+		http.MethodPost:    1,
+		http.MethodPut:     1,
+		http.MethodTrace:   1,
+	}
+}
