7775f15a308a59e5d66eae91617c9863a30b9ae9
John Wang <johncwang@gmail.com>
2021-10-11 03:24:56 -0700

    feat: os/executil/grep: init

---
+++ os/executil/grep/grep.go
@@ -0,0 +1,40 @@
+package grep
+
+import (
+	"strings"
+
+	"github.com/grokify/simplego/type/stringsutil"
+)
+
+type GrepResults []GrepResult
+
+func (gr GrepResults) Files() []string {
+	files := []string{}
+	for _, g := range gr {
+		g.File = strings.TrimSpace(g.File)
+		if len(g.File) > 0 {
+			files = append(files, g.File)
+		}
+	}
+	return stringsutil.SliceCondenseSpace(files, true, true)
+}
+
+type GrepResult struct {
+	File string
+	Line string
+}
+
+// ParseGrep will parse the results from `os/executil.ExecSimple`.
+func ParseGrep(data []byte) GrepResults {
+	res := GrepResults{}
+	lines := strings.Split(string(data), "\n")
+	for _, line := range lines {
+		lineParts := strings.SplitN(string(line), ":", 2)
+		if len(lineParts) == 2 {
+			res = append(res, GrepResult{
+				File: lineParts[0],
+				Line: lineParts[1]})
+		}
+	}
+	return res
+}
