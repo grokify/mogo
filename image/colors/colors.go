fdb5029a6a7a6b7f8ea6712344be93e3f5f555bf
John Wang <johncwang@gmail.com>
2022-01-01 02:11:38 -0800

    feat: image/colors: add `Equal()`, `Red,Green,Blue,Orange`

--- image/colors/colors.go
+++ image/colors/colors.go
@@ -4,6 +4,13 @@ import (
 	"image/color"
 )
 
+var (
+	Red    = color.RGBA{R: 255, G: 0, B: 0, A: 255}
+	Green  = color.RGBA{R: 0, G: 255, B: 0, A: 255}
+	Blue   = color.RGBA{R: 0, G: 0, B: 255, A: 255}
+	Orange = color.RGBA{R: 255, G: 165, B: 0, A: 255}
+)
+
 type Colors []color.Color
 
 func (clrs Colors) In(c color.Color) bool {
@@ -15,4 +22,10 @@ func (clrs Colors) In(c color.Color) bool {
 		}
 	}
 	return false
+}
+
+func Equal(c1, c2 color.Color) bool {
+	r1, g1, b1, a1 := c1.RGBA()
+	r2, g2, b2, a2 := c2.RGBA()
+	return r1 == r2 && g1 == g2 && b1 == b2 && a1 == a2
 }
