ac9d9ee4dc198b49004cb57edc7cb2cee7e55407
John Wang <johncwang@gmail.com>
2022-03-06 00:40:18 -0800

    simplify: imageutil: `BuildGifAnimationSimpleReadAny()`

--- image/imageutil/animate.go
+++ image/imageutil/animate.go
@@ -1,25 +1,16 @@
 package imageutil
 
 import (
-	"fmt"
 	"image"
 	"image/gif"
 )
 
 func BuildGifAnimationSimpleReadAny(src *gif.GIF, delay int, names []string, f ToPalettedFunc) (*gif.GIF, error) {
-	if src == nil {
-		src = &gif.GIF{}
+	imgs, err := ReadImages(names)
+	if err != nil {
+		return nil, err
 	}
-	for i, name := range names {
-		img, _, err := ReadImage(name)
-		if err != nil {
-			return src, fmt.Errorf("image index [%d], cannot be read at location [%s]", i, name)
-		}
-		pimg := imageToPalettedFuncWrap(img, f)
-		src.Image = append(src.Image, pimg)
-		src.Delay = append(src.Delay, delay)
-	}
-	return src, nil
+	return BuildGifAnimationSimple(src, delay, imgs, f), nil
 }
 
 func BuildGifAnimationSimple(src *gif.GIF, delay int, imgs []image.Image, f ToPalettedFunc) *gif.GIF {
