e0f66a62f94c46cc66739a9922720bffd4800549
John Wang <johncwang@gmail.com>
2022-03-07 02:24:15 -0800

    feat: type/number: add `Int32()`, `Int16()`, `Int8()` for safe downcasting

--- type/number/number.go
+++ type/number/number.go
@@ -1,6 +1,8 @@
 package number
 
 import (
+	"errors"
+	"math"
 	"sort"
 	"strconv"
 )
@@ -36,4 +38,27 @@ func IntLength(num int) uint {
 		num = num * -1
 	}
 	return uint(len(strconv.Itoa(num)))
+}
+
+var ErrOverflow = errors.New("integer overflow")
+
+func Int32(i int) (int32, error) {
+	if i > math.MaxInt32 || i < math.MinInt32 {
+		return 0, ErrOverflow
+	}
+	return int32(i), nil
+}
+
+func Int16(i int) (int16, error) {
+	if i > math.MaxInt16 || i < math.MinInt16 {
+		return 0, ErrOverflow
+	}
+	return int16(i), nil
+}
+
+func Int8(i int) (int8, error) {
+	if i > math.MaxInt8 || i < math.MinInt8 {
+		return 0, ErrOverflow
+	}
+	return int8(i), nil
 }
