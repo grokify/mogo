646da14a37dae0ab50c239e5734a54d749cc4e3a
John Wang <johncwang@gmail.com>
2022-03-28 20:44:09 -0700

    feat: maputil: add `MapStringSlice.Sort()`

--- type/maputil/map_string_slice.go
+++ type/maputil/map_string_slice.go
@@ -1,10 +1,23 @@
 package maputil
 
+import (
+	"sort"
+
+	"github.com/grokify/mogo/type/stringsutil"
+)
+
 type MapStringSlice map[string][]string
 
-func (mss MapStringSlice) Add(key, val string) {
-	if _, ok := mss[key]; !ok {
-		mss[key] = []string{}
+func (mss MapStringSlice) Add(key, value string) {
+	mss[key] = append(mss[key], value)
+}
+
+func (mss MapStringSlice) Sort(dedupe bool) {
+	for k, vals := range mss {
+		if dedupe {
+			vals = stringsutil.Dedupe(vals)
+		}
+		sort.Strings(vals)
+		mss[k] = vals
 	}
-	mss[key] = append(mss[key], val)
 }
