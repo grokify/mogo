8566f6b686a3db8adbe5ca0fc571b0cd6b831f79
grokify <johncwang@gmail.com>
2022-04-05 15:32:46 -0700

    feat: osutil: add `FileStatT()`, `MustFileStatT()`

--- os/osutil/fileinfomore.go
+++ os/osutil/fileinfomore.go
@@ -2,6 +2,7 @@ package osutil
 
 import (
 	"os"
+	"syscall"
 	"time"
 )
 
@@ -22,4 +23,17 @@ func NewFileInfoMoreFromPath(path string) (FileInfoMore, error) {
 	fm := FileInfoMore{FileInfo: fi}
 	fm.ModAge = time.Since(fi.ModTime())
 	return fm, nil
+}
+
+func FileStatT(filename string) (syscall.Stat_t, error) {
+	var stat syscall.Stat_t
+	return stat, syscall.Stat(filename, &stat)
+}
+
+func MustFileStatT(filename string) syscall.Stat_t {
+	stat, err := FileStatT(filename)
+	if err != nil {
+		panic(err)
+	}
+	return stat
 }
