2c5b6d948bbaed97d872d75a2d53290b73883aba
John Wang <johncwang@gmail.com>
2022-04-06 01:24:59 -0700

    enhance: cmd: os/osutil/cmd/subdirs: add `--directory` CLI param

--- os/osutil/cmd/subdirs/main.go
+++ os/osutil/cmd/subdirs/main.go
@@ -2,13 +2,25 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/grokify/mogo/log/logutil"
 	"github.com/grokify/mogo/os/osutil"
+	"github.com/jessevdk/go-flags"
 )
 
+type Options struct {
+	Directory string `short:"d" long:"directory" description:"Directory" default:"."`
+}
+
 func main() {
-	err := osutil.VisitPath(osutil.GoPathSrc(), nil, true, false, false, func(dir string) error {
+	opts := Options{}
+	_, err := flags.Parse(&opts)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = osutil.VisitPath(opts.Directory, nil, true, false, false, func(dir string) error {
 		fmt.Println(dir)
 		return nil
 	})
