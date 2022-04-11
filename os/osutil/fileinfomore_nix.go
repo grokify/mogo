8905c594a749bb5baba949d267c1b7a622f0b77f
grokify <johncwang@gmail.com>
2022-04-11 01:24:45 -0700

    feat: osutil: add `SortDirEntriesBirthtimeSec()`

--- os/osutil/fileinfomore_nix.go
+++ os/osutil/fileinfomore_nix.go
@@ -1,8 +1,11 @@
+//go:build linux || darwin
 // +build linux darwin
 
 package osutil
 
 import (
+	"path/filepath"
+	"sort"
 	"syscall"
 )
 
@@ -17,4 +20,13 @@ func MustFileStatT(filename string) syscall.Stat_t {
 		panic(err)
 	}
 	return stat
+}
+
+// SortDirEntriesBirthtimeSec sorts `DirEntries` by `Stat_t.Birthtimespec` which
+// is available on OS-X but not all systems. It will panic if a entry is not found.
+func SortDirEntriesBirthtimeSec(dir string, entries DirEntries) {
+	sort.Slice(entries, func(i, j int) bool {
+		return MustFileStatT(filepath.Join(dir, entries[i].Name())).Birthtimespec.Sec <
+			MustFileStatT(filepath.Join(dir, entries[j].Name())).Birthtimespec.Sec
+	})
 }
