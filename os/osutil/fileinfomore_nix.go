6a4e7c02e3c2ffbd12762d6a53f7eb88bbba5759
grokify <johncwang@gmail.com>
2022-04-11 01:29:32 -0700

    reorg: osutil: extract darwin only code

--- os/osutil/fileinfomore_nix.go
+++ os/osutil/fileinfomore_nix.go
@@ -4,8 +4,6 @@
 package osutil
 
 import (
-	"path/filepath"
-	"sort"
 	"syscall"
 )
 
@@ -20,13 +18,4 @@ func MustFileStatT(filename string) syscall.Stat_t {
 		panic(err)
 	}
 	return stat
-}
-
-// SortDirEntriesBirthtimeSec sorts `DirEntries` by `Stat_t.Birthtimespec` which
-// is available on OS-X but not all systems. It will panic if a entry is not found.
-func SortDirEntriesBirthtimeSec(dir string, entries DirEntries) {
-	sort.Slice(entries, func(i, j int) bool {
-		return MustFileStatT(filepath.Join(dir, entries[i].Name())).Birthtimespec.Sec <
-			MustFileStatT(filepath.Join(dir, entries[j].Name())).Birthtimespec.Sec
-	})
 }
