1a67308dc0fc92a428a8c41f2ea5a0d24fe22997
grokify <johncwang@gmail.com>
2022-05-02 02:42:01 -0700

    housekeeping: strconv/currency: rename file `format.go` from `currency.go`

--- strconv/currency/format.go
+++ strconv/currency/format.go
@@ -1,67 +1,11 @@
 package currency
 
 import (
-	"errors"
 	"strconv"
 	"strings"
 
 	"github.com/leekchan/accounting"
 )
-
-var SymbolMap = map[string]string{
-	"AFN": "؋",
-	"ARS": "$",
-	"AWG": "ƒ",
-	"BRL": "R$",
-	"CAD": "$",
-	"CLP": "$",
-	"CNY": "¥",
-	"CRC": "₡",
-	"CUP": "₱",
-	"EGP": "£",
-	"EUR": "€",
-	"FKP": "£",
-	"GBP": "£",
-	"ILS": "₪",
-	"IRR": "﷼",
-	"JPY": "¥",
-	"KHR": "៛",
-	"KLR": "₨",
-	"KPW": "₩",
-	"KRW": "₩",
-	"MNT": "₮",
-	"NGN": "₦",
-	"NOK": "kr",
-	"PHP": "₱",
-	"PLN": "zł",
-	"RUB": "₽",
-	"SAR": "﷼",
-	"THB": "฿",
-	"UAH": "₴",
-	"USD": "$",
-	"UZS": "лв",
-	"VND": "₫",
-	"YER": "﷼"}
-
-func Symbol(iso4217 string) (string, error) {
-	iso4217 = strings.ToUpper(strings.TrimSpace(iso4217))
-	if sym, ok := SymbolMap[iso4217]; ok {
-		return sym, nil
-	}
-	return iso4217, errors.New("cannot find symbol")
-}
-
-func SymbolPrefix(iso4217 string) string {
-	iso4217 = strings.ToUpper(strings.TrimSpace(iso4217))
-	sym, err := Symbol(iso4217)
-	if err == nil {
-		return sym
-	}
-	if len(iso4217) <= 2 {
-		return iso4217
-	}
-	return strings.Join([]string{strings.ToUpper(iso4217), " "}, "")
-}
 
 func FormatMoney(symbol string, amount float64, precision int) string {
 	ac := accounting.Accounting{Symbol: SymbolPrefix(symbol), Precision: precision}
