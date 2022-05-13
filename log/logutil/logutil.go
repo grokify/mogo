68c075cb2ed7a8e72d1560da657a8bab12f015c6
grokify <johncwang@gmail.com>
2022-05-13 04:57:20 -0700

    enhance: logutil: add `wrap` param to `PrintErr()` and `PrintlnErr()`

--- log/logutil/logutil.go
+++ log/logutil/logutil.go
@@ -17,14 +17,20 @@ func FatalErr(err error, wrap ...string) {
 	}
 }
 
-func PrintErr(err error) {
+func PrintErr(err error, wrap ...string) {
 	if err != nil {
+		for i := len(wrap) - 1; i >= 0; i-- {
+			err = errorsutil.Wrap(err, wrap[i])
+		}
 		log.Print(err.Error())
 	}
 }
 
-func PrintlnErr(err error) {
+func PrintlnErr(err error, wrap ...string) {
 	if err != nil {
+		for i := len(wrap) - 1; i >= 0; i-- {
+			err = errorsutil.Wrap(err, wrap[i])
+		}
 		log.Println(err.Error())
 	}
 }
