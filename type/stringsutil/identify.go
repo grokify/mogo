27e3ca71765077e5a731cbb8e4e4687eb62e2699
John Wang <johncwang@gmail.com>
2022-11-01 02:50:12 -0700

    feat: `stringsutil`: add `IsAlpha()`, `IsAlphaNumeric()`, `IsNumeric()`

--- type/stringsutil/identify.go
+++ type/stringsutil/identify.go
@@ -4,14 +4,25 @@ import (
 	"regexp"
 )
 
-var rxNonDigit = regexp.MustCompile(`[^0-9]`)
-
 func DigitsOnly(input string) string {
-	return rxNonDigit.ReplaceAllString(input, "")
+	return rxNonNumeric.ReplaceAllString(input, "")
 }
 
-var rxDigits = regexp.MustCompile(`^[0-9]+$`)
+var (
+	rxAlpha        = regexp.MustCompile(`^[A-Za-z]+$`)
+	rxAlphaNumeric = regexp.MustCompile(`^[0-9A-Za-z]+$`)
+	rxNumeric      = regexp.MustCompile(`^[0-9]+$`)
+	rxNonNumeric   = regexp.MustCompile(`[^0-9]`)
+)
 
-func IsInteger(input string) bool {
-	return rxDigits.MatchString(input)
+func IsAlpha(s string) bool {
+	return rxAlpha.MatchString(s)
+}
+
+func IsAlphaNumeric(s string) bool {
+	return rxAlphaNumeric.MatchString(s)
+}
+
+func IsNumeric(s string) bool {
+	return rxNumeric.MatchString(s)
 }
