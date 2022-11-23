ba84a5691522bd5355410b2cf70824310e3d1476
John Wang <johncwang@gmail.com>
2022-11-23 06:10:44 -0800

    enhance: `ml/tok2vec`: add `deduplicate` param to `Tok2Vec.Toks2Vecs()`

--- ml/tok2vec/tok2vec.go
+++ ml/tok2vec/tok2vec.go
@@ -6,6 +6,8 @@ import (
 	"github.com/grokify/mogo/type/number"
 )
 
+// Tok2Vec is designed to convert tokens to vectors for multiple documents so that
+// tokens that exist in multiple documents will be assigned a single vector.
 type Tok2Vec struct {
 	toks2VecsMap map[string]int
 	vecs2ToksMap map[int]string
@@ -17,11 +19,13 @@ func NewTok2Vec() Tok2Vec {
 		vecs2ToksMap: map[int]string{}}
 }
 
-func (t2v *Tok2Vec) Toks2Vecs(tokens []string) []int {
+func (t2v *Tok2Vec) Toks2Vecs(tokens []string, deduplicate bool) []int {
 	vecs := []int{}
 	for _, tok := range tokens {
 		if vec, ok := t2v.toks2VecsMap[tok]; ok {
-			vecs = append(vecs, vec)
+			if !deduplicate {
+				vecs = append(vecs, vec)
+			}
 			continue
 		}
 		vec := len(t2v.toks2VecsMap) + 1
@@ -32,8 +36,8 @@ func (t2v *Tok2Vec) Toks2Vecs(tokens []string) []int {
 	return vecs
 }
 
-func (t2v *Tok2Vec) Toks2VecsFloat64(tokens []string) []float64 {
-	return number.SliceToFloat64(t2v.Toks2Vecs(tokens))
+func (t2v *Tok2Vec) Toks2VecsFloat64(tokens []string, deduplicate bool) []float64 {
+	return number.SliceToFloat64(t2v.Toks2Vecs(tokens, deduplicate))
 }
 
 var (
--- ml/tok2vec/tok2vec_test.go
+++ ml/tok2vec/tok2vec_test.go
@@ -7,7 +7,7 @@ import (
 	"github.com/grokify/mogo/text/languageutil"
 )
 
-var t2vTests = []struct {
+var t2vSingleTests = []struct {
 	tok string
 	vec int
 }{
@@ -17,18 +17,57 @@ var t2vTests = []struct {
 	{"Z", 26},
 }
 
-func TestMapStringIntSort(t *testing.T) {
+func TestVec(t *testing.T) {
 	alphabet := strings.Split(languageutil.AlphabetEN, "")
 	t2v := NewTok2Vec()
-	t2v.Toks2Vecs(alphabet)
+	t2v.Toks2Vecs(alphabet, false)
 
-	for _, tt := range t2vTests {
+	for _, tt := range t2vSingleTests {
 		tryVec, err := t2v.Vec(tt.tok)
 		if err != nil {
 			panic(err)
 		}
 		if tryVec != tt.vec {
 			t.Errorf("Tok2Vec.Vec(\"%s\") want [%d], got [%d]", tt.tok, tt.vec, tryVec)
+		}
+	}
+}
+
+var t2vMultiTests = []struct {
+	toks              []string
+	vecsMulti         []int
+	vecsDeduped       []int
+	vecMultiWantErr   bool
+	vecDedupedWantErr bool
+}{
+	{[]string{"Foo", "Bar", "Baz"}, []int{1, 2, 3}, []int{1, 2, 3}, false, false},
+	{[]string{"Foo", "Bar", "Bar", "Qux"}, []int{1, 2, 2, 3}, []int{1, 2, 3}, false, false},
+}
+
+func TestToks2Vecs(t *testing.T) {
+	for _, tt := range t2vMultiTests {
+		t2vMulti := NewTok2Vec()
+		vecsMultiTry := t2vMulti.Toks2Vecs(tt.toks, false)
+		if !tt.vecMultiWantErr && len(vecsMultiTry) != len(tt.vecsMulti) {
+			t.Errorf("Tok2Vec.Toks2Vecs() wrong output count: params [%v, %v] want [%v] got [%v]", tt.toks, false, tt.vecsMulti, vecsMultiTry)
+		} else {
+			for i, vecWant := range tt.vecsMulti {
+				if !tt.vecMultiWantErr && vecWant != vecsMultiTry[i] {
+					t.Errorf("Tok2Vec.Toks2Vecs() wrong output vecs: params [%v, %v] want [%v] got [%v]", tt.toks, false, tt.vecsMulti, vecsMultiTry)
+				}
+			}
+		}
+
+		t2vDeduped := NewTok2Vec()
+		vecsDedupedTry := t2vDeduped.Toks2Vecs(tt.toks, true)
+		if !tt.vecDedupedWantErr && len(vecsDedupedTry) != len(tt.vecsDeduped) {
+			t.Errorf("Tok2Vec.Toks2Vecs() wrong output count: params [%v, %v] want [%v] got [%v]", tt.toks, true, tt.vecsDeduped, vecsDedupedTry)
+		} else {
+			for i, vecWant := range tt.vecsDeduped {
+				if !tt.vecMultiWantErr && vecWant != vecsDedupedTry[i] {
+					t.Errorf("Tok2Vec.Toks2Vecs() wrong output vecs: params [%v, %v] want [%v] got [%v]", tt.toks, true, tt.vecsDeduped, vecsDedupedTry)
+				}
+			}
 		}
 	}
 }
