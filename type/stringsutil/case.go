9c82d48dc0e572ce48c2e4aafce0bbe60175f4ea
John Wang <johncwang@gmail.com>
2022-11-27 07:59:26 -0800

    test: `type/stringsutil`: add tests with empty `Options` slice to `TestEqualFoldFull()`

--- type/stringsutil/case.go
+++ type/stringsutil/case.go
@@ -3,7 +3,7 @@ package stringsutil
 import "golang.org/x/text/cases"
 
 // EqualFoldFull provides "full Unicode case-folding", unlike `strings.EqualFold` which
-// provides "simple Unicode case-folding".
+// provides "simple Unicode case-folding". This is fine to run with no options.
 func EqualFoldFull(s, t string, opts ...cases.Option) bool {
 	c := cases.Fold(opts...)
 	return c.String(s) == c.String(t)
--- type/stringsutil/case_test.go
+++ type/stringsutil/case_test.go
@@ -2,24 +2,34 @@ package stringsutil
 
 import (
 	"testing"
+
+	"golang.org/x/text/cases"
 )
 
 var equalFoldFullTests = []struct {
 	s    string
 	t    string
+	opts []cases.Option
 	want bool
 }{
-	{"grüßen", "GRÜSSEN", true},
-	{"grüßen", "GRÜSSEN ", false},
-	{"grüßen", "GRÜßEN ", false},
+	{"grüßen", "GRÜSSEN", []cases.Option{}, true},
+	{"grüßen", "GRÜSSEN ", []cases.Option{}, false},
+	{"grüßen", "GRÜßEN ", []cases.Option{}, false},
 }
 
 func TestEqualFoldFull(t *testing.T) {
 	for _, tt := range equalFoldFullTests {
-		got := EqualFoldFull(tt.s, tt.t)
+		got := EqualFoldFull(tt.s, tt.t, tt.opts...)
 		if tt.want != got {
 			t.Errorf("stringsutil.EqualFoldFull(\"%s\", \"%s\") Error: want [%v], got [%v]",
 				tt.s, tt.t, tt.want, got)
+		}
+		if len(tt.opts) == 0 {
+			got := EqualFoldFull(tt.s, tt.t)
+			if tt.want != got {
+				t.Errorf("stringsutil.EqualFoldFull(\"%s\", \"%s\") Error: want [%v], got [%v]",
+					tt.s, tt.t, tt.want, got)
+			}
 		}
 	}
 }
