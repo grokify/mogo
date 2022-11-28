8de9d9e2b5931eb05519dfa323d080eb68e6072f
John Wang <johncwang@gmail.com>
2022-11-28 02:50:16 -0800

    test: `type/stringsutil`: enhance `TestEqualFoldFull()` to handle `caser.String()`

--- type/stringsutil/case_test.go
+++ type/stringsutil/case_test.go
@@ -8,23 +8,37 @@ import (
 )
 
 var equalFoldFullTests = []struct {
-	s     string
-	t     string
-	caser cases.Caser
-	want  bool
+	s      string
+	t      string
+	sCased string
+	tCased string
+	caser  cases.Caser
+	want   bool
 }{
-	{"grüßen", "GRÜSSEN", defaultCaser, true},
-	{"grüßen", "GRÜSSEN ", defaultCaser, false},
-	{"grüßen", "GRÜßEN ", defaultCaser, false},
-	{"grüßen", "GRÜSSEN", cases.Title(language.German, cases.NoLower), false},
+	{"grüßen", "GRÜSSEN", "grüssen", "grüssen", defaultCaser, true},
+	{"grüßen", "GRÜSSEN ", "grüssen", "grüssen ", defaultCaser, false},
+	{"grüßen", "GRÜßEN ", "grüssen", "grüssen ", defaultCaser, false},
+	{"grüßen", "GRÜSSEN", "Grüßen", "GRÜSSEN", cases.Title(language.German, cases.NoLower), false},
+	{"grüßen", "Grüßen", "Grüßen", "Grüßen", cases.Title(language.German, cases.NoLower), true},
+	{"GRÜSSEN", "GRÜSSEN", "GRÜSSEN", "GRÜSSEN", cases.Title(language.German, cases.NoLower), true},
 }
 
 func TestEqualFoldFull(t *testing.T) {
-	for _, tt := range equalFoldFullTests {
-		got := EqualFoldFull(tt.s, tt.t, &tt.caser)
-		if tt.want != got {
+	for i, tt := range equalFoldFullTests {
+		gotSCased := tt.caser.String(tt.s)
+		if tt.sCased != gotSCased {
+			t.Errorf("caser.String(\"%s\") S Test [%d] Error: want [%v], got [%v]",
+				tt.s, i, tt.sCased, gotSCased)
+		}
+		gotTCased := tt.caser.String(tt.t)
+		if tt.tCased != gotTCased {
+			t.Errorf("caser.String(\"%s\") T Test [%d] Error: want [%v], got [%v]",
+				tt.t, i, tt.tCased, gotTCased)
+		}
+		gotEqualFold := EqualFoldFull(tt.s, tt.t, &tt.caser)
+		if tt.want != gotEqualFold {
 			t.Errorf("stringsutil.EqualFoldFull(\"%s\", \"%s\") Error: want [%v], got [%v]",
-				tt.s, tt.t, tt.want, got)
+				tt.s, tt.t, tt.want, gotEqualFold)
 		}
 	}
 }
