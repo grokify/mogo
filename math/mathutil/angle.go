f05b2dd3d9afc06fd2b687d4b730ea8665578ce2
John Wang <johncwang@gmail.com>
2022-12-08 01:34:29 -0800

    feat: `math/mathutil`: add `DegreesMinutesSecondsToDecimal()`

--- math/mathutil/angle.go
+++ math/mathutil/angle.go
@@ -9,3 +9,7 @@ func DegreesToRadians(degrees float64) float64 {
 func RadiansToDegrees(radians float64) float64 {
 	return radians * (180 / math.Pi)
 }
+
+func DegreesMinutesSecondsToDecimal(deg, min, sec float64) float64 {
+	return deg + ((min + (sec / 60)) / 60)
+}
