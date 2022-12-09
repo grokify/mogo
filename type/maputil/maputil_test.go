e74b06c0ee779d8e041750a91df0164b4b0785a0
John Wang <johncwang@gmail.com>
2022-12-09 02:33:59 -0800

    cleanup: `type/maputil`: remove `TestMapSSEqual()`

--- type/maputil/maputil_test.go
+++ type/maputil/maputil_test.go
@@ -6,26 +6,6 @@ import (
 	"golang.org/x/exp/slices"
 )
 
-var mapSSEqualTests = []struct {
-	v    map[string]string
-	v2   map[string]string
-	want bool
-}{
-	{map[string]string{"KEY": "num", "VAL": "123"}, map[string]string{"KEY": "num", "VAL": "123"}, true},
-	{map[string]string{"first": "foo", "second": "bar"}, map[string]string{"first": "foo", "second": "baz"}, false},
-}
-
-func TestMapSSEqual(t *testing.T) {
-	for _, tt := range mapSSEqualTests {
-		eq := MapSSEqual(tt.v, tt.v2)
-
-		if eq != tt.want {
-			t.Errorf("maputil.MapMSSEqual() Error: with [%v][%v], want [%v], got [%v]",
-				tt.v, tt.v2, tt.want, eq)
-		}
-	}
-}
-
 var stringKeysExistTests = []struct {
 	v          map[string]any
 	keys       []string
