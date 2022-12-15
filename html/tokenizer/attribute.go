41f8e34ac5fbc13149b589f17ea2a7e22b8e2ea2
John Wang <johncwang@gmail.com>
2022-12-14 17:25:38 -0800

    feat: `html/tokenizer`: add `Attributes.GetVal()`, `Attributes.Has()`

--- html/tokenizer/attribute.go
+++ html/tokenizer/attribute.go
@@ -28,6 +28,25 @@ func (attrs Attributes) GetOne(attributeKey string) (html.Attribute, error) {
 	return matches[0], nil
 }
 
+func (attrs Attributes) GetVal(key string) []string {
+	vals := []string{}
+	for _, a := range attrs {
+		if key == a.Key {
+			vals = append(vals, a.Val)
+		}
+	}
+	return vals
+}
+
+func (attrs Attributes) Has(key, val string) bool {
+	for _, a := range attrs {
+		if a.Key == key && a.Val == val {
+			return true
+		}
+	}
+	return false
+}
+
 func TokenAttribute(token html.Token, attrName string) (string, error) {
 	attrNameWant := strings.TrimSpace(strings.ToLower(attrName))
 	for _, attr := range token.Attr {
