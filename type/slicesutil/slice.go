4c126ae21b344c93c6d20d21932f5e5be68431bb
John Wang <johncwang@gmail.com>
2022-12-26 06:48:52 -0800

    reorg: `slicesutil`: move `MakeMatrix2D()` from `type/generics`

--- type/slicesutil/slice.go
+++ type/slicesutil/slice.go
@@ -13,3 +13,16 @@ func Dedupe[S ~[]E, E comparable](s S) S {
 	}
 	return deduped
 }
+
+// MakeMatrix2D returns a 2-dimensional matrix. Usage as follows
+// `a := Make2D[uint8](dy, dx)`.
+// Sourced from: https://stackoverflow.com/a/71781206/1908967
+func MakeMatrix2D[T any](n, m int) [][]T {
+	matrix := make([][]T, n)
+	rows := make([]T, n*m)
+	for i, startRow := 0, 0; i < n; i, startRow = i+1, startRow+m {
+		endRow := startRow + m
+		matrix[i] = rows[startRow:endRow:endRow]
+	}
+	return matrix
+}
