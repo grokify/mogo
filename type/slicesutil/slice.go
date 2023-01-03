dc24a9a950e5c64b6d68d0acd0858964ee823d47
John Wang <johncwang@gmail.com>
2023-01-03 00:33:07 -0800

    feat: `type/slicesutil`: add `Sub()`, `SubRegexpString()`

--- type/slicesutil/slice.go
+++ type/slicesutil/slice.go
@@ -1,5 +1,12 @@
 package slicesutil
 
+import (
+	"regexp"
+	"sort"
+
+	"golang.org/x/exp/constraints"
+)
+
 // Dedupe returns a string slice with duplicate values removed. First observance is kept.
 func Dedupe[S ~[]E, E comparable](s S) S {
 	deduped := []E{}
@@ -14,12 +21,47 @@ func Dedupe[S ~[]E, E comparable](s S) S {
 	return deduped
 }
 
+// Sub returns a string slice with duplicate values removed. First observance is kept.
+func Sub[S ~[]E, E comparable](s, t S) S {
+	filtered := S{}
+	toRemove := map[E]int{}
+	for _, e := range t {
+		toRemove[e]++
+	}
+	for _, val := range s {
+		if _, ok := toRemove[val]; ok {
+			continue
+		}
+		filtered = append(filtered, val)
+	}
+	return filtered
+}
+
+// Sub returns a string slice with duplicate values removed. First observance is kept.
+func SubRegexpString(s []string, r *regexp.Regexp) []string {
+	filtered := []string{}
+	for _, val := range s {
+		if r.MatchString(val) {
+			continue
+		}
+		filtered = append(filtered, val)
+	}
+	return filtered
+}
+
+// Sort sorts a slice of items that comply wth `constraints.Ordered`.
+func Sort[E constraints.Ordered](s []E) {
+	sort.Slice(s, func(i, j int) bool {
+		return s[i] < s[j]
+	})
+}
+
 // MakeMatrix2D returns a 2-dimensional matrix. Usage as follows
 // `a := Make2D[uint8](dy, dx)`.
 // Sourced from: https://stackoverflow.com/a/71781206/1908967
-func MakeMatrix2D[T any](n, m int) [][]T {
-	matrix := make([][]T, n)
-	rows := make([]T, n*m)
+func MakeMatrix2D[E any](n, m int) [][]E {
+	matrix := make([][]E, n)
+	rows := make([]E, n*m)
 	for i, startRow := 0, 0; i < n; i, startRow = i+1, startRow+m {
 		endRow := startRow + m
 		matrix[i] = rows[startRow:endRow:endRow]
