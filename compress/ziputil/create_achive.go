0f0ef5fa5e406b37ae4cc1c3ba5fdbbb5868950e
John Wang <johncwang@gmail.com>
2023-01-03 00:33:43 -0800

    dep: `compress/ziputil`: update ot use new `DirEntries.Names()`

--- compress/ziputil/create_achive.go
+++ compress/ziputil/create_achive.go
@@ -16,7 +16,7 @@ func ZipFilesRx(zipfile, dir string, rx *regexp.Regexp, removePaths bool) error 
 	if err != nil {
 		return err
 	}
-	filepaths := osutil.DirEntries(entries).Names(dir, true)
+	filepaths := osutil.DirEntries(entries).Names(dir)
 	return ZipFiles(zipfile, removePaths, filepaths)
 }
 
