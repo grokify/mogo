05c5adf1b9397935921350017ae337b258803987
John Wang <johncwang@gmail.com>
2023-01-03 00:31:10 -0800

    feat: `io/ioutil`: add `Write()`

--- io/ioutil/ioutilmore.go
+++ io/ioutil/ioutilmore.go
@@ -1,6 +1,7 @@
 package ioutil
 
 import (
+	"bufio"
 	"bytes"
 	"io"
 )
@@ -209,9 +210,9 @@ func FileinfosNames(fis []os.FileInfo) []string {
 */
 
 // ReaderToBytes reads from an io.Reader, e.g. io.ReadCloser
-func ReaderToBytes(ior io.Reader) ([]byte, error) {
+func ReaderToBytes(r io.Reader) ([]byte, error) {
 	buf := new(bytes.Buffer)
-	_, err := buf.ReadFrom(ior)
+	_, err := buf.ReadFrom(r)
 	if err != nil {
 		return []byte{}, err
 	}
@@ -280,4 +281,24 @@ func ReadAllOrError(r io.Reader) []byte {
 		return []byte(err.Error())
 	}
 	return data
+}
+
+// Write writes from `Writer` to a `Reader`. See `osutil.WriteFileReader()`.
+func Write(w *bufio.Writer, r io.Reader) error {
+	buf := make([]byte, 1024)
+	for {
+		// read a chunk
+		n, err := r.Read(buf)
+		if err != nil && err != io.EOF {
+			return err
+		}
+		if n == 0 {
+			break
+		}
+		// write a chunk
+		if _, err := w.Write(buf[:n]); err != nil {
+			return err
+		}
+	}
+	return w.Flush()
 }
