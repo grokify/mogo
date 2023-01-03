5f5087248c98e02ea976ed0697f2d848686a94fd
John Wang <johncwang@gmail.com>
2023-01-03 00:32:41 -0800

    feat: `type/stringsutil`: add `JoinUint()`

--- type/stringsutil/join/join.go
+++ type/stringsutil/join/join.go
@@ -20,6 +20,15 @@ func JoinInt(a []int, sep string) string {
 	strs := []string{}
 	for _, i := range a {
 		strs = append(strs, strconv.Itoa(i))
+
+	}
+	return strings.Join(strs, sep)
+}
+
+func JoinUint(a []uint, sep string) string {
+	strs := []string{}
+	for _, i := range a {
+		strs = append(strs, fmt.Sprintf("%d", i))
 	}
 	return strings.Join(strs, sep)
 }
