42c980cb960e184a6c513092541915ea79722005
John Wang <johncwang@gmail.com>
2023-01-14 08:42:25 -0800

    reorg: `net/httputilmore`: finalize `net/nethttputil` consolidation

--- net/httputilmore/nethttputil.go
+++ net/httputilmore/nethttputil.go
@@ -1,4 +1,4 @@
-package nethttputil
+package httputilmore
 
 import (
 	"net/http"
@@ -9,23 +9,25 @@ import (
 
 // https://stackoverflow.com/questions/15407719/in-gos-http-package-how-do-i-get-the-query-string-on-a-post-request
 
-func GetReqQueryParam(req *http.Request, headerName string) string {
-	return strings.TrimSpace(req.URL.Query().Get(headerName))
+func GetReqQueryParam(req *http.Request, paramName string) string {
+	return strings.TrimSpace(req.URL.Query().Get(paramName))
 }
 
-func GetSplitReqQueryParam(req *http.Request, headerName, sep string) []string {
+func GetReqQueryParamSplit(req *http.Request, paramName, sep string) []string {
 	return stringsutil.SliceTrimSpace(
 		strings.Split(
-			GetReqQueryParam(req, headerName),
+			GetReqQueryParam(req, paramName),
 			sep),
 		true,
 	)
 }
 
+/*
 type RequestUtil struct {
 	Request *http.Request
 }
 
-func (ru *RequestUtil) QueryParamString(headerName string) string {
-	return strings.TrimSpace(ru.Request.URL.Query().Get(headerName))
+func (ru *RequestUtil) QueryParamString(paramName string) string {
+	return strings.TrimSpace(ru.Request.URL.Query().Get(paramName))
 }
+*/
