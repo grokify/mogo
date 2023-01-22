1c9bd1a9a911c3c8184cfdda2174d8970b60e489
John Wang <johncwang@gmail.com>
2023-01-22 08:14:10 -0800

    test: `time/timeutil`: update `TestSyscall()` for nanoseconds

--- time/timeutil/syscall_test.go
+++ time/timeutil/syscall_test.go
@@ -7,36 +7,37 @@ import (
 )
 
 var syscallTests = []struct {
-	nsec    int64
-	rfc3339 string
+	nsec        int64
+	rfc3339Nano string
 }{
 	{1674382312 * NanosPerSecond, "2023-01-22T10:11:52Z"},
+	{1674382312111222333, "2023-01-22T10:11:52.111222333Z"},
 }
 
 func TestSyscall(t *testing.T) {
 	for _, tt := range syscallTests {
 		ts := syscall.NsecToTimespec(tt.nsec)
 		tsTime := Timespec(ts)
-		tsTry := tsTime.UTC().Format(time.RFC3339)
+		tsTry := tsTime.UTC().Format(time.RFC3339Nano)
 
-		if tsTry != tt.rfc3339 {
-			t.Errorf("Timespec(): nsec [%d] want [%s], got [%s]", tt.nsec, tt.rfc3339, tsTry)
+		if tsTry != tt.rfc3339Nano {
+			t.Errorf("Timespec(): nsec [%d] want [%s], got [%s]", tt.nsec, tt.rfc3339Nano, tsTry)
 		}
 
 		ts2 := syscall.Timespec{Nsec: tt.nsec}
 		ts2Time := Timespec(ts2)
-		ts2Try := ts2Time.UTC().Format(time.RFC3339)
+		ts2Try := ts2Time.UTC().Format(time.RFC3339Nano)
 
-		if ts2Try != tt.rfc3339 {
-			t.Errorf("Timespec(): nsec [%d] want [%s], got [%s]", tt.nsec, tt.rfc3339, ts2Try)
+		if ts2Try != tt.rfc3339Nano {
+			t.Errorf("Timespec(): nsec [%d] want [%s], got [%s]", tt.nsec, tt.rfc3339Nano, ts2Try)
 		}
 
 		tv := syscall.NsecToTimeval(tt.nsec)
 		tvTime := Timeval(tv)
-		tvTry := tvTime.UTC().Format(time.RFC3339)
+		tvTry := tvTime.UTC().Format(time.RFC3339Nano)
 
-		if tsTry != tt.rfc3339 {
-			t.Errorf("Timeval(): nsec [%d] want [%s], got [%s]", tt.nsec, tt.rfc3339, tvTry)
+		if tsTry != tt.rfc3339Nano {
+			t.Errorf("Timeval(): nsec [%d] want [%s], got [%s]", tt.nsec, tt.rfc3339Nano, tvTry)
 		}
 	}
 }
