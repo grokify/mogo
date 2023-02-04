20d7cffd7bc0fa9a29af488ac9f4dfb766899ca3
John Wang <johncwang@gmail.com>
2023-02-04 00:30:48 -0800

    feat: `log/logutil`: add `LogfmtString()`

--- log/logutil/logutil.go
+++ log/logutil/logutil.go
@@ -3,34 +3,50 @@
 package logutil
 
 import (
+	"bufio"
+	"bytes"
 	"log"
 
+	"github.com/go-logfmt/logfmt"
 	"github.com/grokify/mogo/errors/errorsutil"
 )
 
 func FatalErr(err error, wrap ...string) {
+	err = errorsutil.Wrap(err, wrap...)
 	if err != nil {
-		for i := len(wrap) - 1; i >= 0; i-- {
-			err = errorsutil.Wrap(err, wrap[i])
-		}
 		log.Fatal(err.Error())
 	}
 }
 
 func PrintErr(err error, wrap ...string) {
+	err = errorsutil.Wrap(err, wrap...)
 	if err != nil {
-		for i := len(wrap) - 1; i >= 0; i-- {
-			err = errorsutil.Wrap(err, wrap[i])
-		}
 		log.Print(err.Error())
 	}
 }
 
 func PrintlnErr(err error, wrap ...string) {
+	err = errorsutil.Wrap(err, wrap...)
 	if err != nil {
-		for i := len(wrap) - 1; i >= 0; i-- {
-			err = errorsutil.Wrap(err, wrap[i])
-		}
 		log.Println(err.Error())
 	}
+}
+
+func LogfmtString(m map[string][]string) (string, error) {
+	var buf bytes.Buffer
+	w := bufio.NewWriter(&buf)
+	e := logfmt.NewEncoder(w)
+	for k, vs := range m {
+		for _, v := range vs {
+			err := e.EncodeKeyval(k, v)
+			if err != nil {
+				return "", err
+			}
+		}
+	}
+	err := e.EndRecord()
+	if err != nil {
+		return "", err
+	}
+	return buf.String(), nil
 }
