deebd95cd7a2c2d68fd4bf67d76c640e52153432
John Wang <johncwang@gmail.com>
2023-02-04 01:25:38 -0800

    feat: `type/stringsutil`: add `CondenseLines()`

--- type/stringsutil/lines.go
+++ type/stringsutil/lines.go
@@ -19,3 +19,18 @@ func ToLineFeeds(s string) string {
 func SplitLines(s string) []string {
 	return strings.Split(ToLineFeeds(s), "\n")
 }
+
+var rxMultiLinefeeds = regexp.MustCompile(`\n\n\n+`)
+
+// CondenseLines improves text layout by (a) remove staring and trailing spaces from eacn line,
+// (b) remove starting and training new lines before and after characters, and (c) ensuring that
+// there is a max of 2 consecutive line feeds.
+func CondenseLines(s string) string {
+	s = strings.TrimSpace(s)
+	l := strings.Split(s, "\n")
+	for i, li := range l {
+		l[i] = strings.TrimSpace(li)
+	}
+	s = strings.Join(l, "\n")
+	return rxMultiLinefeeds.ReplaceAllString(s, "\n\n")
+}
