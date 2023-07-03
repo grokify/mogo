422ba0d50e5b8d79c541b6729b975266a74609e8
John Wang <johncwang@gmail.com>
2023-07-03 00:30:05 -0700

    enhance: `image/imageutil`: update `cmd/resize_jpeg` to use `ResizePathJPEG()`

--- image/imageutil/cmd/resize_jpeg/main.go
+++ image/imageutil/cmd/resize_jpeg/main.go
@@ -2,27 +2,59 @@ package main
 
 import (
 	"image/jpeg"
+	"log"
 
 	"github.com/grokify/mogo/image/imageutil"
-	"github.com/grokify/mogo/log/logutil"
-	"github.com/jessevdk/go-flags"
+	flags "github.com/jessevdk/go-flags"
 )
 
 type cliOptions struct {
-	Input  string `short:"i" long:"input file" description:"A file" value-name:"FILE" required:"true"`
-	Output string `short:"o" long:"output file" description:"A file" required:"true"`
-	Height uint   `short:"h" long:"height" description:"Height"`
-	Width  uint   `short:"w" long:"width" description:"width"`
+	Input   string `short:"i" long:"input dir/file" description:"A dir or file" value-name:"FILE" required:"true"`
+	Output  string `short:"o" long:"output dir/filefile" description:"A dir or file" required:"true"`
+	Height  uint   `short:"h" long:"height" description:"Height"`
+	Width   uint   `short:"w" long:"width" description:"Width"`
+	Quality int    `short:"q" long:"quality" description:"Quality"`
 }
 
 func main() {
 	opts := cliOptions{}
 	_, err := flags.Parse(&opts)
-	logutil.FatalErr(err)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	err = imageutil.ResizeFileJPEG(opts.Input, opts.Output, opts.Width, opts.Height,
+	err = imageutil.ResizePathJPEG(opts.Input, opts.Output, opts.Width, opts.Height,
 		&imageutil.JPEGEncodeOptions{
-			Options: &jpeg.Options{Quality: imageutil.JPEGQualityMax},
+			Options: &jpeg.Options{Quality: opts.Quality},
 		})
-	logutil.FatalErr(err)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	/*
+		isDirSrc, err := osutil.IsDir(opts.Input)
+		logutil.FatalErr(err)
+
+		if isDirSrc {
+			isDirOut, err := osutil.IsDir(opts.Output)
+			logutil.FatalErr(err)
+			if !isDirOut {
+				logutil.FatalErr(errors.New("output must be a directory"))
+			}
+			// write regexp to match .jpg or .jpeg file extensions
+			files, err := osutil.ReadDirMore(opts.Input, imageutil.RxFileExtensionJPG, false, true, false)
+			logutil.FatalErr(err)
+			n := len(files)
+			for i, e := range files {
+				fmt.Printf("Processing %d of %d: %s\n", i+1, n, e.Name())
+				srcPath := filepath.Join(opts.Input, e.Name())
+				outPath := filepath.Join(opts.Output, e.Name())
+				err := imageutil.ResizeFileJPEG(srcPath, outPath, opts.Width, opts.Height, jopts)
+				logutil.FatalErr(err)
+			}
+		} else {
+			err := imageutil.ResizeFileJPEG(opts.Input, opts.Output, opts.Width, opts.Height, jopts)
+			logutil.FatalErr(err)
+		}
+	*/
 }
