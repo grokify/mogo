e3e9463a9e2271f8d240374a13256da93d81b819
John Wang <johncwang@gmail.com>
2023-08-26 09:19:43 -0700

    feat: `type/slicesutil`: add `IndexValueOrDefault()`

--- type/slicesutil/index.go
+++ type/slicesutil/index.go
@@ -2,9 +2,18 @@ package slicesutil
 
 import "github.com/grokify/mogo/math/mathutil"
 
+// ReverseIndex returns the forward index value from the end of the string.
 func ReverseIndex(n, i uint) uint {
 	if i >= n {
 		i = mathutil.ModPyInt(i, n)
 	}
 	return n - i - 1
+}
+
+// IndexValueOrDefault returns the value at the supplied index or a supplied default value.
+func IndexValueOrDefault[E any](s []E, idx uint, def E) E {
+	if int(idx) >= len(s) {
+		return def
+	}
+	return s[idx]
 }
