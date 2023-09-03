e1567baa14b75b82e0152d4d3dcecae0c0255828
John Wang <johncwang@gmail.com>
2023-09-03 11:00:02 -0700

    feat: `pointer`: add `Dereference()`, `Pointer()`, `Int64()`

--- pointer/simple_create.go
+++ pointer/simple_create.go
@@ -3,13 +3,20 @@
 // 2: https://github.com/openlyinc/pointy
 package pointer
 
-func Int64(num int64) *int64 {
-	return &num
-}
+func Int64(num int64) *int64 { return &num }
+func Pointer[E any](e E) *E  { return &e }
 
 func ToString(s *string) string {
 	if s == nil {
 		return ""
 	}
 	return *s
+}
+
+func Dereference[E any](e *E) E {
+	if e == nil {
+		return *new(E)
+	} else {
+		return *e
+	}
 }
