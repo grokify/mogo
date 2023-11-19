2ee01ca22cb94a4ce1ca7b1f1d26d2762ecb0f89
John Wang <johncwang@gmail.com>
2023-11-19 07:09:49 -0800

    simplify: `time/timeutil`: update calculations for constants

--- time/timeutil/constants.go
+++ time/timeutil/constants.go
@@ -9,9 +9,9 @@ const (
 	DaysPerWeek = float32(7)
 	HoursPerDay = float32(24)
 
-	SecondsPerYear = (365 * 24 * 60 * 60) + (6 * 60 * 60)
-	SecondsPerWeek = 7 * 24 * 60 * 60
 	SecondsPerDay  = 24 * 60 * 60
+	SecondsPerWeek = 7 * SecondsPerDay
+	SecondsPerYear = (365 * SecondsPerDay) + (6 * 60 * 60)
 
 	MonthsEN = `["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]`
 )
