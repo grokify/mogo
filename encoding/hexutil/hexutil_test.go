408ac66bed9b8bf116ab3ae028c9d522cf774b10
John Wang <johncwang@gmail.com>
2024-06-03 01:26:35 -0700

    test: `encoding/hextutil`: add `TestEncodeToString()`, `TestEncodeToStrings()`

---
+++ encoding/hexutil/hexutil_test.go
@@ -0,0 +1,51 @@
+package hexutil
+
+import (
+	"slices"
+	"strconv"
+	"strings"
+	"testing"
+)
+
+var encodeToStringTests = []struct {
+	v    []byte
+	sep  string
+	want string
+}{
+	{[]byte{0xde, 0xad, 0xbe, 0xef}, "", "deadbeef"},
+	{[]byte{0xde, 0xad, 0xbe, 0xef}, " ", "de ad be ef"},
+	{[]byte{0xde, 0xad, 0xbe, 0xef}, ":", "de:ad:be:ef"},
+	{[]byte{0xfe, 0xed, 0xbe, 0xef}, "", "feedbeef"},
+	{[]byte{0xfe, 0xed, 0xbe, 0xef}, " ", "fe ed be ef"},
+	{[]byte{0xfe, 0xed, 0xbe, 0xef}, ":", "fe:ed:be:ef"},
+}
+
+func TestEncodeToString(t *testing.T) {
+	for _, tt := range encodeToStringTests {
+		try := EncodeToString(tt.v, tt.sep)
+		if try != tt.want {
+			t.Errorf("hexutil.EncodeToString(\"%v\",\"%s\") Mismattch: want [%s], got [%s]", tt.v, tt.sep, tt.want, try)
+		}
+	}
+}
+
+var encodeToStringsTests = []struct {
+	v       []byte
+	toUpper bool
+	want    []string
+}{
+	{[]byte{0xde, 0xad, 0xbe, 0xef}, false, []string{"de", "ad", "be", "ef"}},
+	{[]byte{0xde, 0xad, 0xbe, 0xef}, true, []string{"DE", "AD", "BE", "EF"}},
+	{[]byte{0xfe, 0xed, 0xbe, 0xef}, false, []string{"fe", "ed", "be", "ef"}},
+	{[]byte{0xfe, 0xed, 0xbe, 0xef}, true, []string{"FE", "ED", "BE", "EF"}},
+}
+
+func TestEncodeToStrings(t *testing.T) {
+	for _, tt := range encodeToStringsTests {
+		try := EncodeToStrings(tt.v, tt.toUpper)
+		if !slices.Equal(try, tt.want) {
+			t.Errorf("hexutil.EncodeToStrings(\"%v\", %s) Mismattch: want [%s], got [%s]",
+				tt.v, strconv.FormatBool(tt.toUpper), strings.Join(tt.want, " "), strings.Join(try, " "))
+		}
+	}
+}
