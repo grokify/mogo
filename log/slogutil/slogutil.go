5651b53f7c8d59b205a9e77e94305164e8b1d155
John Wang <johncwang@gmail.com>
2024-09-08 15:03:47 -0700

    feat: `log/slogutil`: add `AttrsFromAny()`, `LogOrNotAny()`

--- log/slogutil/slogutil.go
+++ log/slogutil/slogutil.go
@@ -2,6 +2,7 @@ package slogutil
 
 import (
 	"context"
+	"fmt"
 	"log/slog"
 )
 
@@ -12,4 +13,25 @@ func LogOrNot(ctx context.Context, logger *slog.Logger, level slog.Level, msg st
 		ctx = context.Background()
 	}
 	logger.LogAttrs(ctx, level, msg, attrs...)
+}
+
+func LogOrNotAny(ctx context.Context, logger *slog.Logger, level slog.Level, msg string, args ...any) {
+	attrs := AttrsFromAny(args...)
+	LogOrNot(ctx, logger, level, msg, attrs...)
+}
+
+func AttrsFromAny(args ...any) []slog.Attr {
+	var attrs []slog.Attr
+	for i := 0; i < len(args); i += 2 {
+		key, ok := args[i].(string)
+		if !ok {
+			key = fmt.Sprintf("%v", key)
+		}
+		if i < len(args)-1 {
+			attrs = append(attrs, slog.Any(key, args[i+1]))
+		} else {
+			attrs = append(attrs, slog.Any(key, nil))
+		}
+	}
+	return attrs
 }
