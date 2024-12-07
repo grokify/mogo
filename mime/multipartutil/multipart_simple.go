d78c52525dd7b6a522adacd1717241e87caf2bed
John Wang <johncwang@gmail.com>
2024-12-06 20:18:58 -0800

    feat: `mime/multipartutil`: add `MultipartSimple{}`

---
+++ mime/multipartutil/multipart_simple.go
@@ -0,0 +1,71 @@
+package multipartutil
+
+import (
+	"fmt"
+	"strings"
+)
+
+type MultipartSimple struct {
+	Parts []Part
+}
+
+func NewMultipartSimple() MultipartSimple {
+	return MultipartSimple{Parts: []Part{}}
+}
+
+func (ms MultipartSimple) Builder() (MultipartBuilder, error) {
+	mb := NewMultipartBuilder()
+	if len(ms.Parts) == 0 {
+		err := mb.Close()
+		return mb, err
+	}
+	for _, p := range ms.Parts {
+		typ := strings.ToLower(strings.TrimSpace(p.Type))
+		switch typ {
+		case PartTypeFilepath:
+			if err := mb.WriteFilePathPlus(p.Name, p.Filepath, p.Base64Encode); err != nil {
+				return mb, nil
+			}
+		case PartTypeJSON:
+			if err := mb.WriteFieldAsJSON(p.Name, p.Data, p.Base64Encode); err != nil {
+				return mb, nil
+			}
+		case PartTypeString:
+			if err := mb.WriteFieldString(p.Name, p.String); err != nil {
+				return mb, nil
+			}
+		default:
+			return mb, fmt.Errorf("type not supported (%s)", p.Type)
+		}
+	}
+	if err := mb.Close(); err != nil {
+		return mb, err
+	} else {
+		return mb, nil
+	}
+}
+
+func (ms MultipartSimple) Strings() (ctHeader, body string, err error) {
+	mb, err := ms.Builder()
+	if err != nil {
+		return
+	}
+	ctHeader = mb.ContentType()
+	body = mb.String()
+	return
+}
+
+const (
+	PartTypeJSON     = "json"
+	PartTypeFilepath = "filepath"
+	PartTypeString   = "string"
+)
+
+type Part struct {
+	Type         string
+	Name         string
+	Base64Encode bool
+	Data         any
+	Filepath     string
+	String       string
+}
