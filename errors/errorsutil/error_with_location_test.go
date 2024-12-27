dc2b4ba29b6fb22348f39561a47d9529f585aaf1
John Wang <johncwang@gmail.com>
2024-12-27 07:17:27 -0800

    test: `errors/errorsutil`: simplify

--- errors/errorsutil/error_with_location_test.go
+++ errors/errorsutil/error_with_location_test.go
@@ -10,7 +10,7 @@ var errorWithLocationTests = []struct {
 	fn           func() error
 	errMsgSuffix string
 }{
-	{func() error { _, err := time.Parse(time.RFC3339, "Mon, 2024-12-31"); return err }, "github.com/grokify/mogo/errors/errorsutil/error_with_location_test.go:22"},
+	{func() error { _, err := time.Parse(time.RFC3339, "Mon, 2024-12-31"); return err }, `github.com/grokify/mogo/errors/errorsutil/error_with_location_test.go:22)`},
 }
 
 func TestErrorWithLocation(t *testing.T) {
@@ -20,15 +20,8 @@ func TestErrorWithLocation(t *testing.T) {
 			panic("no error")
 		}
 		tryWithLocation := NewErrorWithLocation(try.Error())
-		idx, idxCalc, ok := isSuffixOnly(tryWithLocation.Error(), tt.errMsgSuffix)
-		if !ok {
-			t.Errorf("errorsutil.NewErrorWithLocation(\"%s\"): mismatch want suffix (%s) got (%s), idx (%d) idxCalc (%d)", tryWithLocation.Error(), tt.errMsgSuffix, tryWithLocation.Error(), idx, idxCalc)
+		if !strings.HasSuffix(tryWithLocation.Error(), tt.errMsgSuffix) {
+			t.Errorf("errorsutil.NewErrorWithLocation(\"%s\"): mismatch want suffix [%s] got [%s]", try.Error(), tt.errMsgSuffix, tryWithLocation.Error())
 		}
 	}
-}
-
-func isSuffixOnly(s, substr string) (int, int, bool) {
-	idx := strings.Index(s, substr)
-	idxCalc := len(s) - len(substr) - 1
-	return idx, idxCalc, idx == idxCalc
 }
