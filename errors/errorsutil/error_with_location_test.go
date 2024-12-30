88f9c62b9ab59e0d75af9391fbbfeb8715e20025
John Wang <johncwang@gmail.com>
2024-12-30 15:03:01 -0800

    test: `errors/errorsutil`: update `TestErrorWithLocation()`

--- errors/errorsutil/error_with_location_test.go
+++ errors/errorsutil/error_with_location_test.go
@@ -1,27 +1,27 @@
 package errorsutil
 
 import (
+	"errors"
 	"strings"
 	"testing"
-	"time"
 )
 
 var errorWithLocationTests = []struct {
 	fn           func() error
 	errMsgSuffix string
 }{
-	{func() error { _, err := time.Parse(time.RFC3339, "Mon, 2024-12-31"); return err }, `github.com/grokify/mogo/errors/errorsutil/error_with_location_test.go:22)`},
+	{func() error { return errors.New("test err") }, `github.com/grokify/mogo/errors/errorsutil/error_with_location_test.go:22)`},
 }
 
 func TestErrorWithLocation(t *testing.T) {
 	for _, tt := range errorWithLocationTests {
-		try := tt.fn()
-		if try == nil {
+		tryErr := tt.fn()
+		if tryErr == nil {
 			panic("no error")
 		}
-		tryWithLocation := NewErrorWithLocation(try.Error())
+		tryWithLocation := NewErrorWithLocation(tryErr.Error())
 		if !strings.HasSuffix(tryWithLocation.Error(), tt.errMsgSuffix) {
-			t.Errorf("errorsutil.NewErrorWithLocation(\"%s\"): mismatch want suffix [%s] got [%s]", try.Error(), tt.errMsgSuffix, tryWithLocation.Error())
+			t.Errorf("errorsutil.NewErrorWithLocation(\"%s\"): mismatch want suffix [%s] got [%s]", tryErr.Error(), tt.errMsgSuffix, tryWithLocation.Error())
 		}
 	}
 }
