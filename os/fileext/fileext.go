0035e80c41bf01aac4fb4c196a9cea1d847239c6
John Wang <johncwang@gmail.com>
2025-01-02 18:50:40 -0800

    feat: `os/fileext`: add constants

--- os/fileext/fileext.go
+++ os/fileext/fileext.go
@@ -2,9 +2,12 @@ package fileext
 
 const (
 	ExtDOCX     = "docx"
+	ExtGo       = "go"
 	ExtJPEG     = "jpeg"
 	ExtJSON     = "json"
 	ExtMarkdown = "md"
 	ExtPDF      = "pdf"
 	ExtPNG      = "png"
+	ExtSVG      = "svg"
+	ExtXLSX     = "xlsx"
 )
