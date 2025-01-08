698723660b88644c7aca5aaeb3bab79bd12ac806
John Wang <johncwang@gmail.com>
2025-01-07 19:29:12 -0800

    feat: `net/mailutil`: add constants

---
+++ net/mailutil/constants.go
@@ -0,0 +1,13 @@
+package mailutil
+
+const (
+	HeaderMessageID = "Message-ID"
+	HeaderBcc       = "Bcc"
+	HeaderCc        = "Cc"
+	HeaderFrom      = "From"
+	HeaderSubject   = "Subject"
+	HeaderTo        = "To"
+
+	AddrSep   = ", "
+	HeaderSep = ": "
+)
