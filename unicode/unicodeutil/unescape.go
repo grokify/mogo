d11c7b553bd2e3aad75521a5e4d5f085280c4a51
John Wang <johncwang@gmail.com>
2025-01-25 07:45:47 -0800

    feat: `unicode`: add `UnescapeEach()`

--- unicode/unicodeutil/unescape.go
+++ unicode/unicodeutil/unescape.go
@@ -2,8 +2,11 @@ package unicodeutil
 
 import (
 	"fmt"
+	"regexp"
 	"strconv"
 	"strings"
+
+	"github.com/grokify/mogo/errors/errorsutil"
 )
 
 // Unescape wraps the `strconv.Unquote()` function to provide a method of converting
@@ -15,5 +18,25 @@ func Unescape(s string) (string, error) {
 		return strconv.Unquote(Apostrophe + s + Apostrophe)
 	} else {
 		return s, fmt.Errorf("cannot unescape string (%s)", s)
+	}
+}
+
+var rxUnicode = regexp.MustCompile(`\\u[0-9A-Za-z]{4}`)
+
+func UnescapeEach(s string) (string, error) {
+	var errorsOn []string
+	fn := func(s string) string {
+		if o, err := Unescape(s); err != nil {
+			errorsOn = append(errorsOn, errorsutil.Wrapf(err, "unescape string (%s)", s).Error())
+			return s
+		} else {
+			return o
+		}
+	}
+	out := rxUnicode.ReplaceAllStringFunc(s, fn)
+	if len(errorsOn) > 0 {
+		return out, fmt.Errorf("errors on [%s]", strings.Join(errorsOn, ", "))
+	} else {
+		return out, nil
 	}
 }
