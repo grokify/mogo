c3164c77ac94ed5383550efd7a179d40cbc707cf
John Wang <johncwang@gmail.com>
2025-02-22 23:06:20 -0800

    chore(lint): fix various

--- type/stringsutil/matrix.go
+++ type/stringsutil/matrix.go
@@ -1,16 +1,21 @@
 package stringsutil
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 // Matrix2DColRowIndex returns the row index where the string supplied is first
 // encountered for a supplied column index.
-func Matrix2DColRowIndex[C comparable](mat [][]C, colIdx uint, s C) (int, error) {
-	xint := int(colIdx)
+func Matrix2DColRowIndex[C comparable](mat [][]C, colIdx int, s C) (int, error) {
+	if colIdx < 0 {
+		return -1, errors.New("col index cannot be negative")
+	}
 	for y, row := range mat {
-		if xint >= len(row) {
-			return -1, fmt.Errorf("index out of range [%d] with length %d", xint, len(row))
+		if colIdx >= len(row) {
+			return -1, fmt.Errorf("col index out of range [%d] with length %d", colIdx, len(row))
 		}
-		if row[xint] == s {
+		if row[colIdx] == s {
 			return y, nil
 		}
 	}
