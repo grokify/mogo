57109e575c15742b31cfbe845241b379ca087b7f
John Wang <johncwang@gmail.com>
2025-03-22 04:11:00 -0700

    feat: `net/http/httpsimple`: add `CLI.BodyFilepath`

--- net/http/httpsimple/cli.go
+++ net/http/httpsimple/cli.go
@@ -6,20 +6,23 @@ import (
 	"io"
 	"net/http"
 	"net/url"
+	"os"
 	"strings"
 
 	"github.com/grokify/mogo/net/http/httputilmore"
 	"github.com/grokify/mogo/net/urlutil"
+	"github.com/grokify/mogo/os/osutil"
 )
 
 // CLI executes a HTTP request from CLI params. Attribute tags support usage
 // with `github.com/jessevdk/go-flags`.`
 type CLI struct {
-	Method  string   `short:"M" long:"method" description:"Request Method"`
-	URL     string   `short:"U" long:"url" description:"Reaquest URL"`
-	Header  []string `short:"H" long:"header" description:"Request Header"`
-	Body    string   `short:"B" long:"body" description:"Request Body"`
-	BodyJXU string   `short:"J" long:"jxu" description:"Request Body: JSON Map to URL-encoded"`
+	Method       string   `short:"M" long:"method" description:"Request Method"`
+	URL          string   `short:"U" long:"url" description:"Reaquest URL"`
+	Header       []string `short:"H" long:"header" description:"Request Header"`
+	Body         string   `short:"B" long:"body" description:"Request Body"`
+	BodyFilepath string   `short:"F" long:"filepath" description:"Request Body Filepath"`
+	BodyJXU      string   `short:"J" long:"jxu" description:"Request Body: JSON Map to URL-encoded"`
 }
 
 func (cli CLI) Request() (Request, error) {
@@ -40,13 +43,25 @@ func (cli CLI) Request() (Request, error) {
 		}
 		req.Headers.Add(strings.TrimSpace(parts[0]), strings.TrimSpace(parts[1]))
 	}
-	if strings.TrimSpace(cli.Body) == "" && strings.TrimSpace(cli.BodyJXU) != "" {
-		if uv, err := urlutil.MSABytesToValues([]byte(cli.BodyJXU)); err != nil {
-			return req, err
-		} else if s := uv.Encode(); s != "" {
-			req.Body = s
-			if ct := strings.TrimSpace(req.Headers.Get(httputilmore.HeaderContentType)); ct == "" {
-				req.Headers.Set(httputilmore.HeaderContentType, httputilmore.ContentTypeAppFormURLEncodedUtf8)
+	if strings.TrimSpace(cli.Body) == "" {
+		if strings.TrimSpace(cli.BodyFilepath) != "" {
+			if ok, err := osutil.IsFile(cli.BodyFilepath, true); err != nil {
+				return req, err
+			} else if !ok {
+				return req, fmt.Errorf("filename is not valid or non-zero (%s)", cli.BodyFilepath)
+			} else if b, err := os.ReadFile(cli.BodyFilepath); err != nil {
+				return req, err
+			} else {
+				req.Body = b
+			}
+		} else if strings.TrimSpace(cli.BodyJXU) != "" {
+			if uv, err := urlutil.MSABytesToValues([]byte(cli.BodyJXU)); err != nil {
+				return req, err
+			} else if s := uv.Encode(); s != "" {
+				req.Body = s
+				if ct := strings.TrimSpace(req.Headers.Get(httputilmore.HeaderContentType)); ct == "" {
+					req.Headers.Set(httputilmore.HeaderContentType, httputilmore.ContentTypeAppFormURLEncodedUtf8)
+				}
 			}
 		}
 	}
