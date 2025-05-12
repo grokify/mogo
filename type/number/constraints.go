07fa1b0a72c8de8377ba790feb3694accebf3690
John Wang <johncwang@gmail.com>
2025-05-11 23:40:00 -0700

    feat: `type/number`: add constraints `Integer`, `Signed`, `Unsigned`

---
+++ type/number/constraints.go
@@ -0,0 +1,14 @@
+package number
+
+// Integer is a constraint for all integer types (signed and unsigned).
+type Integer interface {
+	Signed | Unsigned
+}
+
+type Signed interface {
+	~int | ~int8 | ~int16 | ~int32 | ~int64
+}
+
+type Unsigned interface {
+	~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64
+}
