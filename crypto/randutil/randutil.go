e2c53ea94c823543e82dc9e2176ed2186f9caed2
John Wang <johncwang@gmail.com>
2025-05-11 23:34:40 -0700

    feat: `crypto/randutil`: add `CryptoRandIntInRange()`

--- crypto/randutil/randutil.go
+++ crypto/randutil/randutil.go
@@ -3,52 +3,12 @@ package randutil
 import (
 	crand "crypto/rand"
 	"encoding/binary"
+	"errors"
+	"math"
 	mrand "math/rand"
+
+	"github.com/grokify/mogo/type/number"
 )
-
-/*
-type CryptoRand struct {
-	reader io.Reader
-	mrand  *mrand.Rand
-}
-
-func NewCryptoRand(r io.Reader, s mrand.Source) CryptoRand {
-	if r == nil {
-		r = rand.Reader
-	}
-	if s == nil {
-		s = mrand.NewSource(time.Now().Unix())
-	}
-	return CryptoRand{
-		reader: r,
-		mrand:  mrand.New(s)} // #nosec G404
-}
-
-func (cr *CryptoRand) Intn(n int) (int, error) {
-	i64, err := cr.Int64n(int64(n))
-	return int(i64), err
-}
-
-func (cr *CryptoRand) MustIntn(n int) int {
-	return int(cr.MustInt64n(int64(n)))
-}
-
-func (cr *CryptoRand) Int64n(n int64) (int64, error) {
-	bign, err := rand.Int(cr.reader, big.NewInt(n))
-	if err != nil {
-		return -1, err
-	}
-	return bign.Int64(), nil
-}
-
-func (cr *CryptoRand) MustInt64n(n int64) int64 {
-	i64, err := cr.Int64n(n)
-	if err != nil {
-		return int64(cr.mrand.Intn(int(n)))
-	}
-	return i64
-}
-*/
 
 func Float64() (float64, error) {
 	var b [8]byte
@@ -73,4 +33,56 @@ func MustFloat64() float64 {
 // Intn returns a random number backed by `crypto/rand`.
 func Intn(n int) int {
 	return mrand.New(NewCryptoRandSource()).Intn(n) // #nosec G404 - `NewCryptoRandSource()` uses `crypto/rand`.
+}
+
+// CryptoRandIntInRange returns a cryptographically secure random integer in [min, max] (inclusive).
+func CryptoRandIntInRange[T number.Integer](min, max T) (T, error) {
+	if min > max {
+		return 0, errors.New("min must be <= max")
+	}
+	span := uint64(max-min) + 1
+	if span == 0 {
+		return 0, errors.New("span overflow")
+	}
+
+	var nBytes int
+	var maxUint uint64
+	switch any(min).(type) {
+	case int8, uint8:
+		nBytes = 1
+		maxUint = math.MaxUint8
+	case int16, uint16:
+		nBytes = 2
+		maxUint = math.MaxUint16
+	case int32, uint32:
+		nBytes = 4
+		maxUint = math.MaxUint32
+	case int64, uint64, int, uint:
+		nBytes = 8
+		maxUint = math.MaxUint64
+	default:
+		return 0, errors.New("unsupported type")
+	}
+
+	limit := maxUint - (maxUint % span)
+	b := make([]byte, nBytes)
+	for {
+		if _, err := crand.Read(b); err != nil {
+			return 0, err
+		}
+		var n uint64
+		switch nBytes {
+		case 1:
+			n = uint64(b[0])
+		case 2:
+			n = uint64(binary.BigEndian.Uint16(b))
+		case 4:
+			n = uint64(binary.BigEndian.Uint32(b))
+		case 8:
+			n = binary.BigEndian.Uint64(b)
+		}
+		if n < limit {
+			return min + T(n%span), nil
+		}
+	}
 }
