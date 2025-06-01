e9057d87242767c93e2f955969714a1921a23652
John Wang <johncwang@gmail.com>
2025-06-01 15:55:51 -0700

    feat: `type/maputil.MapCompInt{}`: add `Percentages()`, `PercentagesInt()`

--- type/maputil/map_comp_int.go
+++ type/maputil/map_comp_int.go
@@ -4,6 +4,30 @@ import "strconv"
 
 type MapCompInt[C comparable] map[C]int
 
+func (mci MapCompInt[C]) Percentages() map[C]float32 {
+	out := map[C]float32{}
+	sum := 0
+	for _, v := range mci {
+		sum += v
+	}
+	for k, v := range mci {
+		out[k] = float32(v) / float32(sum)
+	}
+	return out
+}
+
+func (mci MapCompInt[C]) PercentagesInt() map[C]int {
+	out := map[C]int{}
+	sum := 0
+	for _, v := range mci {
+		sum += v
+	}
+	for k, v := range mci {
+		out[k] = int(100 * (float32(v) / float32(sum)))
+	}
+	return out
+}
+
 func (mci MapCompInt[C]) ReverseCounts() map[int]int {
 	out := map[int]int{}
 	for _, v := range mci {
