10a1881bfe4dbc9e655ba3d9c104ba5f7820926c
John Wang <johncwang@gmail.com>
2025-07-13 05:51:05 -0700

    feat: `unicode/unicodeutil`: add `RemoveDiacritics()`

--- unicode/unicodeutil/unicodeutil.go
+++ unicode/unicodeutil/unicodeutil.go
@@ -3,7 +3,31 @@ package unicodeutil
 import (
 	"strconv"
 	"strings"
+	"unicode"
+
+	"golang.org/x/text/unicode/norm"
 )
+
+// RemoveDiacritics removes diacritical marks (accents) from a Unicode string.
+// A rune map can be used for manual overrides such as converting '™' to "tm".
+func RemoveDiacritics(s string, m map[rune][]rune) string {
+	t := norm.NFD.String(s)
+	b := make([]rune, 0, len(t))
+
+	for _, r := range t {
+		if v, ok := m[r]; ok && len(v) > 0 {
+			b = append(b, v...)
+		} else if ok {
+			continue
+		} else if unicode.Is(unicode.Mn, r) {
+			continue // skip mark (Mn) runes
+		} else {
+			b = append(b, r)
+		}
+	}
+
+	return string(b)
+}
 
 // Unescape wraps the `strconv.Unquote()` function to provide a method of converting
 // \u escaped unicode literals. It can take a string like "M\\u00fcnchen" and return "München"
--- unicode/unicodeutil/unicodeutil_test.go
+++ unicode/unicodeutil/unicodeutil_test.go
@@ -4,15 +4,35 @@ import (
 	"testing"
 )
 
-var unescapeTests = []struct {
-	v    string
-	want string
-}{
-	{`M\u00fcnchen`, "München"},
-	{`"M\u00fcnchen"`, `"München"`},
+// TestRemoveDiacritics ensures timeutil.DateDMYHM2 is parsed to GMT timezone.
+func TestRemoveDiacritics(t *testing.T) {
+	var removeDiacriticsTests = []struct {
+		v    string
+		vMap map[rune][]rune
+		want string
+	}{
+		{"João", nil, "Joao"},
+		{"München", nil, "Munchen"},
+		{"item™", map[rune][]rune{'™': {'t', 'm'}}, "itemtm"}, // mkdocs example
+	}
+
+	for _, tt := range removeDiacriticsTests {
+		try := RemoveDiacritics(tt.v, tt.vMap)
+		if try != tt.want {
+			t.Errorf("unicodeutil.RemoveDiacritics(\"%s\") Mismatch: want [%s], got [%s]", tt.v, tt.want, try)
+		}
+	}
 }
 
 func TestUnescape(t *testing.T) {
+	var unescapeTests = []struct {
+		v    string
+		want string
+	}{
+		{`M\u00fcnchen`, "München"},
+		{`"M\u00fcnchen"`, `"München"`},
+	}
+
 	for _, tt := range unescapeTests {
 		unescaped, err := Unescape(tt.v)
 		if err != nil {
