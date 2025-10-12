9855e452e1ae952d5b5991f42954a73f0e2d203d
John Wang <johncwang@gmail.com>
2025-10-12 09:02:54 -0700

    test: `text/markdown`: update to full width separator line

---
+++ text/markdown/table_gfm_test.go
@@ -0,0 +1,35 @@
+package markdown
+
+import (
+	"strings"
+	"testing"
+)
+
+func TestTableRowsToMarkdown(t *testing.T) {
+	var markdownFormatTests = []struct {
+		columns      []string
+		rows         [][]string
+		sepLineIndex int
+	}{
+		{[]string{"a", "a"}, [][]string{{"bb", "bbb"}, {"cccc", "ccccc"}}, 1},
+	}
+
+	for _, tt := range markdownFormatTests {
+		allRows := [][]string{tt.columns}
+		allRows = append(allRows, tt.rows...)
+
+		md := TableRowsToMarkdown(allRows, "\n", true, true)
+
+		lines := strings.Split(md, "\n")
+
+		length := 0
+		for i, l := range lines {
+			if i == 0 {
+				length = len(l)
+			} else if len(l) != length {
+				t.Errorf("Table.Markdown() Mismatch: first line length (%d) line (%d) length (%d)",
+					length, i, len(l))
+			}
+		}
+	}
+}
