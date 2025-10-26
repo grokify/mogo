b7f5e087f07a864ef402929793c93686bc809d5c
John Wang <johncwang@gmail.com>
2025-10-25 17:43:02 -0700

    feat `time/duration`: add `Spec{}`

---
+++ time/duration/spec.go
@@ -0,0 +1,44 @@
+package duration
+
+import (
+	"fmt"
+	"time"
+)
+
+type Unit string
+
+const (
+	SlugYear   Unit = "year"
+	SlugMonth  Unit = "month"
+	SlugWeek   Unit = "week"
+	SlugDay    Unit = "day"
+	SlugHour   Unit = "hour"
+	SlugMinute Unit = "minute"
+	SlugSecond Unit = "second"
+
+	SlugBusinessDay = "businessday"
+)
+
+type Spec struct {
+	Value int64
+	Unit  Unit
+}
+
+func (s Spec) Duration() (time.Duration, error) {
+	switch s.Unit {
+	case SlugYear:
+		return time.Duration(s.Value) * Year, nil
+	case SlugMonth:
+		return time.Duration(s.Value) * 30 * Day, nil
+	case SlugDay:
+		return time.Duration(s.Value) * Day, nil
+	case SlugHour:
+		return time.Duration(s.Value) * time.Hour, nil
+	case SlugMinute:
+		return time.Duration(s.Value) * time.Minute, nil
+	case SlugSecond:
+		return time.Duration(s.Value) * time.Second, nil
+	default:
+		return 0, fmt.Errorf("unknown time duration unit (%s)", string(s.Unit))
+	}
+}
